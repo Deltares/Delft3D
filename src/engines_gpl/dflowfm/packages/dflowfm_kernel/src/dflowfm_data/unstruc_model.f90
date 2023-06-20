d00d7d45b292960825696042410b23f66e0b5e72
Bea Saggiorato <beatrice.saggiorato@deltares.nl>
2023-06-20 16:28:29 +0200

    UNST-6771: added "_" between date and timeSolved TODO in time_module

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/unstruc_model.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/unstruc_model.f90
@@ -1643,7 +1643,9 @@ subroutine readMDUFile(filename, istat)
     Startdatetime = ' '
     call prop_get_string  (md_ptr, 'time', 'Startdatetime', Startdatetime, success)
     if (len_trim(Startdatetime) > 0 .and. success) then
-        call datetimestring_to_seconds(Startdatetime, refdat, tim, iostat)
+        tmpstr = ''
+        tmpstr  = Startdatetime(1:8)//'_'//Startdatetime(9:14)
+        call datetimestring_to_seconds(tmpstr, refdat, tim, iostat)
         if (iostat == 0) then
            Tstart_user = tim
         endif
@@ -1652,7 +1654,9 @@ subroutine readMDUFile(filename, istat)
     Stopdatetime = ' '
     call prop_get_string  (md_ptr, 'time', 'Stopdatetime', Stopdatetime, success)
     if (len_trim(Stopdatetime) > 0 .and. success) then
-        call datetimestring_to_seconds(Stopdatetime, refdat, tim, iostat)
+        tmpstr = ''
+        tmpstr  = Stopdatetime(1:8)//'_'//Stopdatetime(9:14)
+        call datetimestring_to_seconds(tmpstr, refdat, tim, iostat)
         if (iostat == 0) then
            Tstop_user = tim
         endif
