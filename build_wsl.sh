2e4d67a56e9f9d1031a211e5f8c6b03c6208730f
Floris Buwalda <buwalda@v-slurmsub001.directory.intra>
2023-09-18 10:29:45 +0200

    can compile DflowFM on Alma8

--- build_wsl.sh
+++ build_wsl.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #. /usr/share/Modules/init/bash
 
-source /opt/intel/oneapi/setvars.sh
-export PKG_CONFIG_PATH=/mnt/c/Development/petsc-3.19.4/arch-linux-c-opt/lib/pkgconfig/
-cmake ./src/cmake -DCMAKE_Fortran_COMPILER=mpiifort -D CONFIGURATION_TYPE:STRING=DflowFM -D CMAKE_BUILD_TYPE=Debug -B build_all
+module load intel/2023.1.0
+module load intelmpi/2021.10.0
+module load netcdf/4.9.2_4.6.1_intel2023.1.0
+module load gdal/3.6.3_intel2023.1.0
+module load gcc/12.2.0_gcc12.2.0
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/u/buwalda/petsc/arch-linux-c-opt/lib/pkgconfig/
+
+cmake ./src/cmake -DCMAKE_Fortran_COMPILER=mpiifort -D CONFIGURATION_TYPE:STRING=All -D CMAKE_BUILD_TYPE=Debug -B build_all
 
 cd build_all
-cmake --build . -j --target install --config Debug
+cmake --build . -j --target install --config Release
