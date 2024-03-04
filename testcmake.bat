addb5bfca955cab54473695e6944d6c19de4fc6f
FlorisBuwaldaDeltares <117371974+FlorisBuwaldaDeltares@users.noreply.github.com>
2024-03-04 15:39:50 +0100

    remove testcmake.bat

--- testcmake.bat
+++
@@ -1,5 +0,0 @@
-"C:\Program Files\CMake\bin\cmake.exe" ./src/cmake -D CMAKE_BUILD_TYPE=Debug -D CONFIGURATION_TYPE:STRING=delft3d4 -B build_test -D CMAKE_INSTALL_PREFIX=.\build_test\x64\Debug\
- 
-cd build_test
-"C:\Program Files\CMake\bin\cmake.exe" --build . --target install -j --config Debug > out.txt
-cd ..
