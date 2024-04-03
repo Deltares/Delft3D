ec8b1548adcdd3087611a1f4e2a56105f5463753
mKlapwijk <maarten.klapwijk@deltares.nl>
2024-04-03 12:50:51 +0200

    add copyright headers

--- src/engines_gpl/waq/waq_process/d-emissions/calcorder.f90
+++ src/engines_gpl/waq/waq_process/d-emissions/calcorder.f90
@@ -1,22 +1,29 @@
+!!  Copyright (C)  Stichting Deltares, 2012-2024.
+!!
+!!  This program is free software: you can redistribute it and/or modify
+!!  it under the terms of the GNU General Public License version 3,
+!!  as published by the Free Software Foundation.
+!!
+!!  This program is distributed in the hope that it will be useful,
+!!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+!!  GNU General Public License for more details.
+!!
+!!  You should have received a copy of the GNU General Public License
+!!  along with this program. If not, see <http://www.gnu.org/licenses/>.
+!!
+!!  contact: delft3d.support@deltares.nl
+!!  Stichting Deltares
+!!  P.O. Box 177
+!!  2600 MH Delft, The Netherlands
+!!
+!!  All indications and logos of, and references to registered trademarks
+!!  of Stichting Deltares remain the property of Stichting Deltares. All
+!!  rights reserved.
+
+
 subroutine calcorder(noseg,downstream,simorder)   
-!**********************************************************************
-!     +------------------------+
-!     |    D E L T A R E S     |
-!     +------------------------+
-!
-!***********************************************************************
-!
-!     Project : wflow - EM
-!     Author  : Jos van Gils
-!     Date    : 27032023           Version : 1.00
-!
-!     History :
-!
-!     Date    Author          Description
-!     ------  --------------  -----------------------------------
-!     270323  Jos van Gils    First version
-!***********************************************************************
-!
+!<
 !     Description of the module :
 !
 !        Determine an order of simulation of a WFLOW grid
