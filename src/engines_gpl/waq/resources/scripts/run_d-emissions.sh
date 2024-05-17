548e264e4b55dba2c35a233db76bd8704bc87ca6
Michelle Jeuken <michelle.jeuken@deltares.nl>
2024-05-17 12:58:01 +0200

    D-11 Added D-Emissions run scripts, and added copy actions to install

---
+++ src/engines_gpl/waq/resources/scripts/run_d-emissions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -V
+#$ -j yes
+#$ -cwd
+    #
+    # This script runs D-Emissions on Linux
+    #
+scriptdirname=`readlink \-f \$0`
+scriptdir=`dirname $scriptdirname`
+D3D_HOME=$scriptdir/..
+bindir=$D3D_HOME/bin
+procfile=$D3D_HOME/share/d-emissions/em_proc_def
+
+$bindir/run_delwaq.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -p "$procfile"
