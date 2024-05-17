38e94b6721c5826a58a8153b7de21e3274f17e24
mKlapwijk <maarten.klapwijk@deltares.nl>
2024-05-17 15:38:22 +0200

    correct dwaves config

--- src/cmake/configurations/dwaves_configuration.cmake
+++ src/cmake/configurations/dwaves_configuration.cmake
@@ -86,6 +86,13 @@ if(WIN32)
     endif()
 endif()
 
+# proj
+if(WIN32)
+    if(NOT TARGET proj)
+        include(${CMAKE_SOURCE_DIR}/src/cmake/configurations/include/proj_configuration.cmake)
+    endif()
+endif(WIN32)
+
 # kdtree2
 if(NOT TARGET kdtree2)
     add_subdirectory(${CMAKE_SOURCE_DIR}/src/${kdtree_module} kdtree2)
