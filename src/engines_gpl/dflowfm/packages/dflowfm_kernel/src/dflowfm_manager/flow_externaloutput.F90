4e25fe5f44bab243e24c57e8736527a7f745b9e9
willem4 <willem.ottevanger@gmail.com>
2024-07-16 21:35:23 +0200

    write initial time when jaeverydt==1

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_manager/flow_externaloutput.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_manager/flow_externaloutput.F90
@@ -129,7 +129,6 @@
            end if
         end if
 
-        if (jaeverydt == 0) then
            if (jamapFlowAnalysis > 0) then
               ! update the cumulative flow analysis parameters, and also compute the right CFL numbers
               call updateFlowAnalysisParameters()
@@ -144,7 +143,6 @@
               limitingTimestepEstimation = 0
               flowCourantNumber = 0d0
            endif
-        endif
          if (comparereal(time_map, ti_mape, eps10) == 0) then
             time_map = tstop_user  + 1
          else
