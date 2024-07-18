76ef612f58ea76acf166427082da6dd2c2c72ea6
willem4 <willem.ottevanger@gmail.com>
2024-07-18 10:03:21 +0200

    undo accidental commit

--- test/deltares_testbench/get_testcases_per_xml.py
+++
@@ -1,8 +0,0 @@
-import os
-
-with open(r'd:\tmp\Check_Duplicates\premerge-xml.txt', 'r') as f: 
-    for line in f:
-        xmlfile = line.strip()
-        print(f'python Testbench.py --list --config configs/dimr/{xmlfile} --filter "program=dflowfm:testcase=e02" --skip-download all --skip-run > {xmlfile}.txt 2>&1')
-        os.system(f'python Testbench.py --list --config configs/dimr/{xmlfile} --filter "program=dflowfm:testcase=e02" --skip-download all --skip-run > {xmlfile}.txt 2>&1')
-
