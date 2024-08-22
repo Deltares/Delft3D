f3187be6b1543cc4eb2a01dcee738189a5aa3e75
FlorisBuwaldaDeltares <117371974+FlorisBuwaldaDeltares@users.noreply.github.com>
2024-08-22 12:42:09 +0200

    remove accidentally committed batch files

--- buildcmake.bat
+++
@@ -1,6 +0,0 @@
-cmake ./src/cmake -D CMAKE_BUILD_TYPE=Debug -T fortran=ifort -D CONFIGURATION_TYPE:STRING=all -B build_all -D CMAKE_INSTALL_PREFIX=D:\checkouts\Trunk_git\test\deltares_testbench\data\engines\teamcity_artifacts\x64\
-pause
-rem python .\src\convert_vfproj_to_use_ifx.py ifx build_delft3d4
-
-rem cd build_all
-rem cmake --build . -j --target install --config Debug  
--- test/deltares_testbench/runtests.bat
+++
@@ -1,2 +0,0 @@
-python TestBench.py --compare --config configs\dimr\dimr_dflowfm_win64_parallel.xml --filter "testcase=e02_f012_c0322" --log-level DEBUG --parallel 
-pause
