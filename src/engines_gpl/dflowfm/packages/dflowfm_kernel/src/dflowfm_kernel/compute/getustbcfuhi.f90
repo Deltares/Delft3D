b21f333dc9ee8690b294ba772e5fec873e7692d8
JulienGroenenboom <Julien.Groenenboom@deltares.nl>
2024-09-15 14:35:55 +0200

    UNST-7650: Start with main, apply changes from Herman (from !739) instead of solving merge conflicts.

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/getustbcfuhi.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/getustbcfuhi.f90
@@ -118,13 +118,10 @@
 
          umod = sqrt(u1Lb * u1Lb + v(Lb) * v(Lb))
          ! updated ustokes needed before conversion to eulerian velocities
-         if (jawave > 0 .and. .not. flowwithoutwaves) then
+         if (jawavestokes > 0 .and. .not. flowwithoutwaves) then
             ! get ustar wave squared, fw and wavedirection cosines based upon Swart, ustokes
             call getustwav(LL, z00, fw, ustw2, csw, snw, Dfu, Dfuc, deltau, costu, uorbu)
-            !
-            if (jawaveStokes >= 1) then ! ustokes correction at bed
-               umod = sqrt((u1Lb - ustokes(Lb)) * (u1Lb - ustokes(Lb)) + (v(Lb) - vstokes(Lb)) * (v(Lb) - vstokes(Lb)))
-            end if
+            umod = sqrt((u1Lb - ustokes(Lb)) * (u1Lb - ustokes(Lb)) + (v(Lb) - vstokes(Lb)) * (v(Lb) - vstokes(Lb)))
          end if
 
          if (umod == 0d0) then ! from dry to wet
@@ -135,7 +132,7 @@
 
          ustbLL = sqcf * umod ! ustar based upon bottom layer/layer integral velocity
 
-         if (jawave > 0 .and. .not. flowWithoutWaves) then
+         if (jawavestokes > 0 .and. .not. flowWithoutWaves) then
             rhoL = rhomean ! for now
             if (ustw2 > 1d-8) then
                !
@@ -218,7 +215,7 @@
                if (stm_included) wblt(LL) = deltau
                !
                ! Streaming below deltau with linear distribution
-               if (jawavestreaming == 1 .and. deltau > 1d-7) then ! Streaming below deltau with linear distribution
+               if (jawavestreaming > 0 .and. deltau > 1d-7) then ! Streaming below deltau with linear distribution
                   Dfu0 = Dfuc ! (m/s2)
                   do L = Lb, Ltop(LL)
                      if (hu(L) <= deltau) then
@@ -258,20 +255,13 @@
             z0urou(LL) = z0ucur(LL) ! morfo, bedforms, trachytopes
          end if
 
-         if (jawave > 0 .and. jawaveStokes >= 1 .and. .not. flowWithoutWaves) then ! Ustokes correction at bed
-            adve(Lb) = adve(Lb) - cfuhi3D * ustokes(Lb)
-         end if
-
       else if (ifrctyp == 10) then ! Hydraulically smooth, glass etc
          nit = 0
          u1Lb = u1(Lb)
          umod = sqrt(u1Lb * u1Lb + v(Lb) * v(Lb))
-         if (jawave > 0) then
+         if (jawavestokes > 0 .and. .not. flowwithoutwaves) then
             call getustwav(LL, z00, fw, ustw2, csw, snw, Dfu, Dfuc, deltau, costu, uorbu) ! get ustar wave squared, fw and wavedirection cosines based upon Swart, ustokes
-            !
-            if (jawaveStokes >= 1) then
-               umod = sqrt((u1Lb - ustokes(Lb)) * (u1Lb - ustokes(Lb)) + (v(Lb) - vstokes(Lb)) * (v(Lb) - vstokes(Lb))) ! was ustokes(LL)
-            end if
+            umod = sqrt((u1Lb - ustokes(Lb)) * (u1Lb - ustokes(Lb)) + (v(Lb) - vstokes(Lb)) * (v(Lb) - vstokes(Lb))) ! was ustokes(LL)
          end if
 
          r = umod * hu(Lb) / viskin ! Local re-number:
