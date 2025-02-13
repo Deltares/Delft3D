34f067dd1b54210be45970cf20c4a3cfb2436d8f
Matthijs Huisman <matthijs.huisman@deltares.nl>
2025-02-13 08:59:13 +0100

    add working bat script instead of sh script

--- ci/teamcity/Delft3D/windows/scripts/setTimeParam.bat
+++ ci/teamcity/Delft3D/windows/scripts/setTimeParam.bat
@@ -1,6 +1,18 @@
-#!/bin/bash
-# Get the commit time of the current commit
-GIT_HEAD_TIME=$(git show -s --format=%ci HEAD)
+@echo off
+REM Check if Git is installed
+git --version >nul 2>&1
+IF ERRORLEVEL 1 (
+    echo Git is not installed or not found in PATH. Please install Git and try again.
+    exit /b 1
+)
 
-# Set the TeamCity parameter
-echo "##teamcity[setParameter name=env.TIME_ISO_8601 value=${GIT_HEAD_TIME}]"
+REM Get the commit time of the current commit
+git show -s --format=%%ci HEAD > temp.txt
+SET /P GIT_HEAD_TIME=<temp.txt
+DEL temp.txt
+
+REM Set the TeamCity parameter
+echo ##teamcity[setParameter name='env.TIME_ISO_8601' value='%GIT_HEAD_TIME%']
+
+REM Remove the variable
+set GIT_HEAD_TIME=
