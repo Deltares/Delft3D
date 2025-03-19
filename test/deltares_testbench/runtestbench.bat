2af80ca6587d6fe7341cdbb40b8d584adffb0e28
buwalda <floris.buwalda@deltares.nl>
2025-03-19 13:30:21 +0100

    remove accidentally committed batch files

--- test/deltares_testbench/runtestbench.bat
+++
@@ -1,2 +0,0 @@
-python TestBench.py --compare --config configs\dimr/dimr_dflowfm_drtc_general_win64.xml --filter "testcase=e105_f04_c103_orifice_crestLevel_TimeController" --log-level DEBUG --parallel
-pause
