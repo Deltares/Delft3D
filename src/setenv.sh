71748be8aaae3b93670f43c2786074fb23d67038
reyns <johan.reyns@deltares.nl>
2025-04-29 14:09:43 +0200

    Remove frac from bermslope fluxes, is already fraction specific

--- src/setenv.sh
+++ src/setenv.sh
@@ -84,6 +84,7 @@ else
      echo "Loading Intel23 compiled modules"
   
      module load intel/2023.1.0
+	 module load intelmkl/2023.1.0
      module load intelmpi/2021.10.0
      module load netcdf/4.9.2_4.6.1_intel2023.1.0
      module load gdal/3.6.3_intel2023.1.0
