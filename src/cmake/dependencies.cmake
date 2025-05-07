129d289ce8075f0c51feed873b2ef2b3d1082f9a
Hidde Elzinga <hidde.elzinga@deltares.nl>
2025-05-07 12:00:00 +0200

    Fix linux build

--- src/cmake/dependencies.cmake
+++ src/cmake/dependencies.cmake
@@ -1,8 +1,8 @@
 # NetCDF
-find_package(NetCDF REQUIRED)
+find_package(netCDF REQUIRED)
 find_package(netcdf-fortran REQUIRED)
 
 # geo spatial
-find_package(gdal REQUIRED)
-find_package(proj REQUIRED)
+find_package(GDAL REQUIRED)
+find_package(PROJ REQUIRED)
 find_package(shapelib REQUIRED)
