05bf5527d2194fee1e89702db8f2f730f4d1f410
Robin van Westrenen <robin.vanwestrenen@deltares.nl>
2025-05-09 11:51:31 +0000

    Add tools_gpl too

--- src/cmake/configurations/testbench/wave_configuration.cmake
+++ src/cmake/configurations/testbench/wave_configuration.cmake
@@ -5,6 +5,9 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/components/drr_configuration.
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/components/fbc_configuration.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/miscellaneous/tools_configuration.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/miscellaneous/windows_postbuild_configuration.cmake)
+
+# For d3d4(+waves) support.
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/miscellaneous/tools_gpl_configuration.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/components/d_hydro_configuration.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/components/rtc_configuration.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/components/flow2d3d_configuration.cmake)
