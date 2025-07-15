05690d5c7b0edeb14be8104dc6f6b0b53640b391
Willem Ottevanger <willem.ottevanger@deltares.nl>
2025-07-15 12:28:18 +0200

    update testcase xml and add run script

---
+++ src/utils_lgpl/nefis/scripts/run_test.bat
@@ -0,0 +1,35 @@
+@ echo off
+title run_nefis_test
+rem
+rem this script runs a nefis test on Windows
+rem
+setlocal enabledelayedexpansion
+
+rem show usage?
+if [%1] EQU [-h]      goto usage
+if [%1] EQU [--help]  goto usage
+if [%1] EQU [--usage] goto usage
+
+rem Set the directories containing the binaries and set PATH
+set testdir=%~dp0
+set libdir=%testdir%\..\..\lib
+set PATH=%libdir%;%testdir%;%PATH%
+
+rem run nefis test
+"%testdir%\test_%1.exe" 
+goto end
+
+:usage
+echo Purpose: 
+echo Runs Nefis tests
+echo.
+echo Usage:
+echo run_test.bat [test number] [-h ^| --help ^| --usage]
+echo.
+echo     test number
+echo     -h ^| --help ^| --usage show this usage (optional)
+echo.
+:end
+
+rem To prevent the DOS box from disappearing immediately: remove the rem on the following line
+rem pause
