cc68c345e4f489b42151197aa813c51ae262cf72
Robin van Westrenen <robin.vanwestrenen@deltares.nl>
2025-07-30 23:15:41 +0200

    Fix common.sh to work from everywhere.

--- src/engines_gpl/dimr/scripts/generic/lnx64/common.sh
+++ src/engines_gpl/dimr/scripts/generic/lnx64/common.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
+# Get the directory where the script is located
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
 _debug() {
     shift  # Skip the "debug" argument
     echo ======================= DEBUG ==========================
     for prog in "$@"; do
         echo
         echo === ${prog} -v
-        ${prog} -v
+        "${SCRIPT_DIR}/${prog}" -v
         echo
         echo === ldd ${prog}
-        ldd ${prog}
+        ldd "${SCRIPT_DIR}/${prog}"
         echo
         echo === ldd lib${prog}.so
-        ldd ../lib/lib${prog}.so
+        ldd "${SCRIPT_DIR}/../lib/lib${prog}.so"
         echo
     done
     echo ========================================================
 }
 
-# Check for deactivation parameter
+# Check for debug parameter
 if [ "$1" == "debug" ]; then
     _debug "$@"
 fi
