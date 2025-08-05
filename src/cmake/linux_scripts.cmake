d86c06cb73e82e8939fa53199e7da9c06b45ad3f
René van Klink <rene.vanklink@deltares.nl>
2025-08-05 09:55:46 +0000

    UNST-8896: Update comment in linux_scripts.cmake

--- src/cmake/linux_scripts.cmake
+++ src/cmake/linux_scripts.cmake
@@ -2,12 +2,15 @@
 set(CMAKE_INSTALL_RPATH "$ORIGIN:$ORIGIN/../lib")
 
 # use, i.e. don't skip the full RPATH for the build tree
-SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
+set(CMAKE_SKIP_BUILD_RPATH  FALSE)
 
-# when building, don't use the install RPATH already
-# (but later on when installing)
-SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+# When building, don't use the "install" RPATH yet. During the install phase,
+# the binaries will be updated to use the "install" RPATH.
+# Setting this option to "FALSE" ensures that, for example, the unit test
+# executables that depend on libraries in the build folder can run without
+# requiring an install and/or changing LD_LIBRARY_PATH.
+set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
 
-SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-rpath='$ORIGIN:$ORIGIN/../lib' -Wl,--enable-new-dtags")
 
