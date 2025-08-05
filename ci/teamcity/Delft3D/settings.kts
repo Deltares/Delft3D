910aa0b4666c4b1bf9cbb544409fcdd52a70b3da
Mike de Jong <mike.dejong@deltares.nl>
2025-08-05 15:37:16 +0200

    Remove keeper connection

--- ci/teamcity/Delft3D/settings.kts
+++ ci/teamcity/Delft3D/settings.kts
@@ -162,12 +162,5 @@ project {
             allowInSubProjects = true
             allowInBuilds = true
         }
-        feature {
-            id = "KEEPER_VAULT_DELFT3D"
-            type = "JetBrains.KeeperVault"
-            param("vaultUrl", "https://keepersecurity.eu/vault")
-            param("clientId", "credentialsJSON:75f914cc-ec3c-4028-84c5-51f9ab6d10d2")
-            param("privateKey", "credentialsJSON:2ebd62a7-eda9-49fd-b1a2-21d4ebdec32a")
-        }
     }
 }
