956ca27faa217ec3fc98f9df410ddfcf11133adc
huisman <matthijs.huisman@deltares.nl>
2025-08-28 08:55:27 +0000

    disable stuff to explicitly test docker connection

--- ci/teamcity/Delft3D/verschilanalyse/ReportVerschilanalyse.kt
+++ ci/teamcity/Delft3D/verschilanalyse/ReportVerschilanalyse.kt
@@ -47,60 +47,6 @@ object ReportVerschilanalyse: BuildType({
                 --volume="%env.AWS_SHARED_CREDENTIALS_FILE%:/root/.aws/credentials:ro"
             """.trimIndent()
         }
-        script {
-            name = "Unzip logs and verschillentool output"
-            val script = File(DslContext.baseDir, "verschilanalyse/scripts/unzip_reports.sh")
-            scriptContent = Util.readScript(script)
-        }
-        python {
-            name = "Generate summaries"
-            pythonVersion = customPython {
-                executable = "python3.11"
-            }
-            environment = venv {
-                requirementsFile = ""
-                pipArgs = "--editable ./ci/python[verschilanalyse]"
-            }
-            command = module {
-                module = "ci_tools.verschilanalyse.generate_summaries"
-                scriptArguments = """
-                    --current-log-dir=current_logs
-                    --reference-log-dir=reference_logs
-                    --verschillen-dir=verschillen
-                    --output-dir=summaries
-                    --s3-current-prefix=s3://devops-test-verschilanalyse/%current_prefix%
-                    --s3-reference-prefix=s3://devops-test-verschilanalyse/%reference_prefix%
-                    --report-build-url=%env.TEAMCITY_SERVER_URL%/buildConfiguration/%system.teamcity.buildType.id%/%teamcity.build.id%
-                    --artifact-base-url=%env.TEAMCITY_SERVER_URL%/repository/download/%system.teamcity.buildType.id%/%teamcity.build.id%:id
-                """.trimIndent()
-            }
-        }
-        python {
-            name = "Send email"
-            executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
-            pythonVersion = customPython {
-                executable = "python3.11"
-            }
-            environment = venv {
-                requirementsFile = ""
-                pipArgs = "--editable ./ci/python[verschilanalyse]"
-            }
-            command = module {
-                module = "ci_tools.verschilanalyse.send_verschilanalyse_email"
-                scriptArguments = """
-                    --email-server=%env.EMAIL_SERVER%
-                    --email-port=%env.EMAIL_PORT%
-                    --email-from=%env.EMAIL_FROM%
-                    --email-to=%env.EMAIL_TO%
-                    --email-content=summaries/email_content.html
-                    --attachment=summaries/verschillentool_summary.xlsx
-                """.trimIndent()
-            }
-        }
-    }
-
-    dependencies {
-        snapshot(StartVerschilanalyse) {}
     }
 
     features {
@@ -113,12 +59,6 @@ object ReportVerschilanalyse: BuildType({
             loginToRegistry = on {
                 dockerRegistryId = "DOCKER_REGISTRY_DELFT3D"
             }
-        }
-        xmlReport {
-            reportType = XmlReport.XmlReportType.JUNIT
-            rules = """
-                +:summaries/verschilanalyse_junit.xml
-            """.trimIndent()
         }
     }
 
