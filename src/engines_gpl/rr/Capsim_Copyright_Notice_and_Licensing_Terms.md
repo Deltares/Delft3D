723ad5906d43350b73fa7276ec17f9528218cf0a
Arthur van Dam <arthurvd@gmail.com>
2025-10-08 09:56:58 +0200

    UNST-9059: Renamed capsim license md file to LICENSE-CAPSIM.txt and changed to official MIT license
    + added MIT header into capsim.f90 itself.

--- src/engines_gpl/rr/Capsim_Copyright_Notice_and_Licensing_Terms.md
+++
@@ -1,5 +0,0 @@
-**Copyright Notice and Licensing Terms**
-
-The software known as CAPSIM is protected under copyright law, with rights originating in the year 1995. All intellectual property rights in and to CAPSIM are owned by the DLO Winand Staring Centre for Integrated Land, Soil and Water Research (SC-DLO), located at P.O. Box 125, NL-6700 AC Wageningen, The Netherlands.
-
-Pursuant to written authorization granted by the DLO Winand Staring Centre, CAPSIM is distributed and integrated under the same licensing terms as those applicable to the D-Flow FM software.
--- src/engines_gpl/rr/packages/rr_kernel_f/src/capsim.f90
+++ src/engines_gpl/rr/packages/rr_kernel_f/src/capsim.f90
@@ -1,18 +1,25 @@
-!----- AGPL ---------------------------------------------------------------------
-!
-!  Copyright (C)  Stichting Deltares, 2011-2025.
-!
-!  This program is free software: you can redistribute it and/or modify
-!  it under the terms of the GNU Affero General Public License as
-!  published by the Free Software Foundation version 3.
-!
-!  This program is distributed in the hope that it will be useful,
-!  but WITHOUT ANY WARRANTY; without even the implied warranty of
-!  MERCHANTABILITY or FITNESS For A PARTICULAR PURPOSE.  See the
-!  GNU Affero General Public License for more details.
-!
-!  You should have received a copy of the GNU Affero General Public License
-!  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+! MIT License
+! 
+! Copyright (c) 2025 Stichting Deltares
+! Copyright (c) 1995 DLO Winand Staring Centre for Integrated Land, Soil and Water Research (SC-DLO)
+! 
+! Permission is hereby granted, free of charge, to any person obtaining a copy
+! of this software and associated documentation files (the "Software"), to deal
+! in the Software without restriction, including without limitation the rights
+! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+! copies of the Software, and to permit persons to whom the Software is
+! furnished to do so, subject to the following conditions:
+! 
+! The above copyright notice and this permission notice shall be included in all
+! copies or substantial portions of the Software.
+! 
+! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+! SOFTWARE.
 !
 !  contact: delft3d.support@deltares.nl
 !  Stichting Deltares
