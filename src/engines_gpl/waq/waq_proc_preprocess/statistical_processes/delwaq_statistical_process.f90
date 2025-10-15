fbc9595dc42a73d03b226c7057c140d490263e4b
Robin <Robin.vanWestrenen@deltares.nl>
2025-10-15 11:19:44 +0200

    Revert "DELWAQ-1143 Adding STAMEA, a light version of STADSC with only mean as output"

--- src/engines_gpl/waq/waq_proc_preprocess/statistical_processes/delwaq_statistical_process.f90
+++ src/engines_gpl/waq/waq_proc_preprocess/statistical_processes/delwaq_statistical_process.f90
@@ -27,7 +27,6 @@ module m_delwaq_statistical_process
    use m_setprc
    use m_setgeo
    use m_setdsc
-   use m_setmea
    use m_setdpt
    use m_setday
    use m_rdstat
@@ -133,11 +132,6 @@ contains
                nsproc = nsproc + nperiod
                goto 10
             end if
-            key = 'STAMEA'
-            if (string_equals(key, keyval(ikey))) then
-               nsproc = nsproc + nperiod
-               goto 10
-            end if
             key = 'STAGEO'
             if (string_equals(key, keyval(ikey))) then
                nsproc = nsproc + nperiod
@@ -223,17 +217,6 @@ contains
                   write (lunrep, '(3a)') 'For period [', pernam(iperiod), ']:'
                   isproc = isproc + 1
                   call setdsc(lunrep, nokey(istat), keynam2(ikstat), keyval2(ikstat), pernam(iperiod), persfx(iperiod), &
-                              pstart(iperiod), pstop(iperiod), isproc, aprocesprop, allitems, status)
-                  iret = procespropcolladd(statprocesdef, aprocesprop)
-               end do
-               goto 100
-            end if
-            key = 'STAMEA'
-            if (string_equals(key, keyval(ikey))) then
-               do iperiod = 1, nperiod
-                  write (lunrep, '(3a)') 'For period [', pernam(iperiod), ']:'
-                  isproc = isproc + 1
-                  call setmea(lunrep, nokey(istat), keynam2(ikstat), keyval2(ikstat), pernam(iperiod), persfx(iperiod), &
                               pstart(iperiod), pstop(iperiod), isproc, aprocesprop, allitems, status)
                   iret = procespropcolladd(statprocesdef, aprocesprop)
                end do
