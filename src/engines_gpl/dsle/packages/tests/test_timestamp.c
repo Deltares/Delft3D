7b3f60b15ad4c8454ec19ff6dbad29fb7829a348
René van Klink <rene.vanklink@deltares.nl>
2025-10-25 12:07:36 +0200

    UNST-8589: Use a different approach to compute the tz offset that should work on both Windows and Linux.

--- src/engines_gpl/dsle/packages/tests/test_timestamp.c
+++ src/engines_gpl/dsle/packages/tests/test_timestamp.c
@@ -9,10 +9,20 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-static int get_tz_offset(time_t time) {
-  struct tm buf;
-  localtime_r(&time, &buf);
-  return buf.tm_gmtoff;
+// `timegm` is implemented in the Windows C standard library as `_mkgmtime`.
+// So unfortunately we need a platform dependent macro if we want to use `timegm`.
+#ifdef _WIN32
+#define timegm _mkgmtime
+#endif
+
+static int get_tz_offset_seconds(time_t epoch_time) {
+  // Convert seconds since Unix epoch to the "local calendar time" (Y-M-D, h:s:m).
+  // Note: `localtime` adds the timezone offset from the system's locale (taking into account daylight saving time).
+  struct tm *local_calendar_time = localtime(&epoch_time);
+  // Convert the "local calendar time" back to seconds since Unix epoch, while pretending that you're someone living in the UTC timezone.
+  time_t confused_epoch_time = timegm(local_calendar_time);
+  // The difference between the `confused_epoch_time` and the input `epoch_time` is actually the timezone offset in seconds.
+  return confused_epoch_time - epoch_time;
 }
 
 static void test_timestamp_conversion(void) {
@@ -21,7 +31,7 @@ static void test_timestamp_conversion(void) {
   double local_timestamp_as_double = time_to_timestamp(time_minus_local_tz);
   time_t time_at_utc = 1726668240;
 
-  int local_tz_offset = get_tz_offset(time_minus_local_tz);
+  int local_tz_offset = get_tz_offset_seconds(time_minus_local_tz);
   TEST_ASSERT_EQUAL_INT64(time_at_utc, time_minus_local_tz + local_tz_offset);
   TEST_ASSERT_EQUAL_DOUBLE(202409181404.0, local_timestamp_as_double);
   TEST_ASSERT_EQUAL_INT64(time_at_utc, timestamp_to_time(local_timestamp_as_double) + local_tz_offset);
@@ -31,7 +41,7 @@ static void test_timestamp_advance(void) {
   double local_timestamp = 202409181326.0;
   time_t t1_minus_local_tz = timestamp_to_time(local_timestamp);
   time_t t1_at_utc = 1726665960;
-  int local_tz_offset = get_tz_offset(t1_minus_local_tz);
+  int local_tz_offset = get_tz_offset_seconds(t1_minus_local_tz);
   TEST_ASSERT_EQUAL_INT64(t1_at_utc, t1_minus_local_tz + local_tz_offset);
 
   time_t t2 = t1_minus_local_tz + 60;
@@ -47,10 +57,10 @@ static void test_timestamp_arrays(void) {
   time_t *t_arr = timestamp_array_to_times(d_arr, 3);
   time_t t_refs[3];
   for (int i = 0; i < 3; ++i) {
-    t_refs[i] = t_arr[i] + get_tz_offset(t_arr[i]);
+    t_refs[i] = t_arr[i] + get_tz_offset_seconds(t_arr[i]);
   }
 
-  TEST_ASSERT_INT64_ARRAY_WITHIN(0, t_refs, t_arr, 3);
+  TEST_ASSERT_INT64_ARRAY_WITHIN(0, t_at_utc, t_refs, 3);
   // Result should be strictly increasing.
   TEST_ASSERT(times_strictly_increasing(t_arr, 3) == 1);
   // Non strictly increasing times.
