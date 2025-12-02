cdd426930d07a9b3b807b800e3cc727ec8a6d011
FlorisBuwaldaDeltares <floris.buwalda@Deltares.nl>
2025-12-02 16:11:23 +0100

    small fixes

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/pol_to_cellmask.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/pol_to_cellmask.F90
@@ -46,8 +46,7 @@ module m_pol_to_cellmask
    ! Module-level variables for cellmask polygon checking
    real(kind=dp), allocatable :: xpmin_cellmask(:), ypmin_cellmask(:)
    real(kind=dp), allocatable :: xpmax_cellmask(:), ypmax_cellmask(:)
-   real(kind=dp), allocatable :: zpl_start_cellmask(:)
-   real(kind=dp), allocatable :: xpl_cellmask(:), ypl_cellmask(:)
+   real(kind=dp), allocatable :: xpl_cellmask(:), ypl_cellmask(:), zpl_cellmask(:)
    integer, allocatable :: iistart_cellmask(:), iiend_cellmask(:)
    integer :: Npoly_cellmask = 0
    logical :: cellmask_initialized = .false.
@@ -55,21 +54,21 @@ module m_pol_to_cellmask
 contains
 
    subroutine pol_to_cellmask()
-   use network_data, only: cellmask, nump1d2d, npl, nump, xzw, yzw, xpl, ypl, zpl
-   use m_partitioninfo, only: jampi
+      use network_data, only: cellmask, nump1d2d, npl, nump, xzw, yzw, xpl, ypl, zpl
+      use m_partitioninfo, only: jampi
 #ifdef _OPENMP
       use omp_lib
       integer :: temp_threads
 #endif
-   integer :: k
-   if (allocated(cellmask)) deallocate(cellmask)
-   allocate(cellmask(nump1d2d))
-   cellmask = 0
+      integer :: k
+      if (allocated(cellmask)) deallocate (cellmask)
+      allocate (cellmask(nump1d2d))
+      cellmask = 0
 
-   if (NPL == 0) return
+      if (NPL == 0) return
 
-   ! Initialize once
-   call dbpinpol_cellmask_init(NPL, xpl, ypl, zpl)
+      ! Initialize once
+      call dbpinpol_cellmask_init(NPL, xpl, ypl, zpl)
 #ifdef _OPENMP
       temp_threads = omp_get_max_threads() !> Save old number of threads
       if (jampi == 0) then
@@ -85,12 +84,12 @@ contains
       call omp_set_num_threads(temp_threads)
 #endif
 
-   ! Cleanup
-   call dbpinpol_cellmask_cleanup()
+      ! Cleanup
+      call dbpinpol_cellmask_cleanup()
 
-end subroutine pol_to_cellmask
+   end subroutine pol_to_cellmask
 
-  !> Initialize polygon data structures for cellmask checking.
+   !> Initialize polygon data structures for cellmask checking.
    !! Must be called once before using dbpinpol_cellmask.
    subroutine dbpinpol_cellmask_init(NPL, xpl, ypl, zpl)
       use m_alloc
@@ -114,6 +113,10 @@ end subroutine pol_to_cellmask
          return
       end if
 
+      ! Allocate and copy full coordinate arrays
+      xpl_cellmask = xpl
+      ypl_cellmask = ypl
+
       MAXPOLY = 1000
       call realloc(xpmin_cellmask, maxpoly, keepExisting=.false.)
       call realloc(xpmax_cellmask, maxpoly, keepExisting=.false.)
@@ -121,7 +124,7 @@ end subroutine pol_to_cellmask
       call realloc(ypmax_cellmask, maxpoly, keepExisting=.false.)
       call realloc(iistart_cellmask, maxpoly, keepExisting=.false.)
       call realloc(iiend_cellmask, maxpoly, keepExisting=.false.)
-      call realloc(zpl_start_cellmask, maxpoly, keepExisting=.false.)
+      call realloc(zpl_cellmask, maxpoly, keepExisting=.false.)
 
       ipoint = 1
       ipoly = 0
@@ -136,7 +139,7 @@ end subroutine pol_to_cellmask
             call realloc(ypmax_cellmask, maxpoly, keepExisting=.true.)
             call realloc(iistart_cellmask, maxpoly, keepExisting=.true.)
             call realloc(iiend_cellmask, maxpoly, keepExisting=.true.)
-            call realloc(zpl_start_cellmask, maxpoly, keepExisting=.true.)
+            call realloc(zpl_cellmask, maxpoly, keepExisting=.true.)
          end if
 
          ! Get polygon start and end pointer
@@ -154,34 +157,31 @@ end subroutine pol_to_cellmask
 
          iistart_cellmask(ipoly) = istart
          iiend_cellmask(ipoly) = iend
-         
+
          ! Store polygon type (sign of zpl)
-         if (zpl(istart) /= dmiss) then
-            zpl_start_cellmask(ipoly) = zpl(istart)
-         else
-            zpl_start_cellmask(ipoly) = dmiss
-         end if
+         zpl_cellmask(ipoly) = zpl(istart)
 
          ! Advance pointer
          ipoint = iend + 2
       end do
-      
+
       Npoly_cellmask = ipoly
       cellmask_initialized = .true.
 
    end subroutine dbpinpol_cellmask_init
 
-  !> Check if point should be masked.
+   !> Check if point should be masked.
    elemental function dbpinpol_cellmask(xp, yp) result(mask)
-      use m_missing, only: dmiss  !< Use globals directly
+      use m_missing, only: dmiss
       implicit none
 
       integer :: mask
       real(kind=dp), intent(in) :: xp, yp
 
-      integer :: istart, iend, ipoly, in_test
+      integer :: ipoly, in_test
       logical :: found_inside_enclosure
       integer :: num_enclosures
+      real(kind=dp) :: zpl_val
 
       mask = 0
       if (.not. cellmask_initialized) return
@@ -189,34 +189,36 @@ end subroutine pol_to_cellmask
       found_inside_enclosure = .false.
       num_enclosures = 0
 
+      ! Single loop over all polygons
       do ipoly = 1, Npoly_cellmask
-         istart = iistart_cellmask(ipoly)
-         iend = iiend_cellmask(ipoly)
+         zpl_val = zpl_cellmask(ipoly)
 
-         if (zpl_start_cellmask(ipoly) == dmiss) cycle
-
-         ! Bounding box check
+         ! Bounding box check (applies to all polygon types)
          if (xp < xpmin_cellmask(ipoly) .or. xp > xpmax_cellmask(ipoly) .or. &
              yp < ypmin_cellmask(ipoly) .or. yp > ypmax_cellmask(ipoly)) cycle
 
-         ! Point-in-polygon test using globals dmiss and JINS
-         in_test = pinpok_elemental(xp, yp, iend - istart + 1)
+         ! Point-in-polygon test
+         in_test = pinpok_elemental(xp, yp, ipoly)
 
-         if (zpl_start_cellmask(ipoly) > 0.0_dp) then
-            ! Dry point polygon
+         if (zpl_val == dmiss .or. zpl_val > 0.0_dp) then
+            ! Dry point polygon (including dmiss): inside = masked
             if (in_test == 1) then
                mask = 1
-               return
+               return ! Early exit
             end if
-         else if (zpl_start_cellmask(ipoly) < 0.0_dp) then
-            ! Enclosure polygon
+         else if (zpl_val < 0.0_dp) then
+            ! Enclosure polygon: track if we're inside any enclosure
             num_enclosures = num_enclosures + 1
-            if (in_test == 1) found_inside_enclosure = .true.
+            if (in_test == 1) then
+               found_inside_enclosure = .true.
+            end if
          end if
       end do
 
-      ! Outside all enclosures = dry
-      if (num_enclosures > 0 .and. .not. found_inside_enclosure) mask = 1
+      ! After checking all polygons: outside all enclosures = dry
+      if (num_enclosures > 0 .and. .not. found_inside_enclosure) then
+         mask = 1
+      end if
 
    end function dbpinpol_cellmask
 
@@ -224,14 +226,14 @@ end subroutine pol_to_cellmask
    subroutine dbpinpol_cellmask_cleanup()
       implicit none
 
-      if (allocated(xpmin_cellmask)) deallocate(xpmin_cellmask)
-      if (allocated(xpmax_cellmask)) deallocate(xpmax_cellmask)
-      if (allocated(ypmin_cellmask)) deallocate(ypmin_cellmask)
-      if (allocated(ypmax_cellmask)) deallocate(ypmax_cellmask)
-      if (allocated(zpl_start_cellmask)) deallocate(zpl_start_cellmask)
-      if (allocated(iistart_cellmask)) deallocate(iistart_cellmask)
-      if (allocated(iiend_cellmask)) deallocate(iiend_cellmask)
-      
+      if (allocated(xpmin_cellmask)) deallocate (xpmin_cellmask)
+      if (allocated(xpmax_cellmask)) deallocate (xpmax_cellmask)
+      if (allocated(ypmin_cellmask)) deallocate (ypmin_cellmask)
+      if (allocated(ypmax_cellmask)) deallocate (ypmax_cellmask)
+      if (allocated(zpl_cellmask)) deallocate (zpl_cellmask)
+      if (allocated(iistart_cellmask)) deallocate (iistart_cellmask)
+      if (allocated(iiend_cellmask)) deallocate (iiend_cellmask)
+
       Npoly_cellmask = 0
       cellmask_initialized = .false.
 
@@ -240,23 +242,23 @@ end subroutine pol_to_cellmask
    !> Optimized elemental point-in-polygon test using ray casting algorithm.
    !! Accesses polygon data via module arrays.
    elemental function pinpok_elemental(xl, yl, ipoly) result(inside)
-      use m_missing, only: dmiss, JINS  !< Use globals directly
+      use m_missing, only: dmiss, JINS !< Use globals directly
 
       implicit none
 
-      real(kind=dp), intent(in) :: xl, yl     !< Point coordinates (scalar)
-      integer, intent(in) :: ipoly            !< Polygon index
-      integer :: inside                       !< Result: 1=inside, 0=outside     
+      real(kind=dp), intent(in) :: xl, yl !< Point coordinates (scalar)
+      integer, intent(in) :: ipoly !< Polygon index
+      integer :: inside !< Result: 1=inside, 0=outside
 
       integer :: i, j, istart, iend, crossings
       real(kind=dp) :: x1, x2, y1, y2, xinters
 
       inside = 0
-      
+
       ! Get polygon bounds from module variables
       istart = iistart_cellmask(ipoly)
       iend = iiend_cellmask(ipoly)
-      
+
       if (iend - istart + 1 <= 2) then
          inside = 1
          goto 999
@@ -265,25 +267,25 @@ end subroutine pol_to_cellmask
       ! Ray-casting algorithm
       crossings = 0
       j = iend
-      
+
       do i = istart, iend
          if (xpl_cellmask(i) == dmiss) exit
-         
+
          x1 = xpl_cellmask(j)
          y1 = ypl_cellmask(j)
          x2 = xpl_cellmask(i)
          y2 = ypl_cellmask(i)
-         
+
          ! Check if point is on vertex
          if (xl == x1 .and. yl == y1) then
             inside = 1
             goto 999
          end if
-         
+
          ! Check if ray crosses edge
          if ((y1 > yl) .neqv. (y2 > yl)) then
             xinters = x1 + (yl - y1) * (x2 - x1) / (y2 - y1)
-            
+
             if (xl < xinters) then
                crossings = crossings + 1
             else if (xl == xinters) then
@@ -295,7 +297,7 @@ end subroutine pol_to_cellmask
       end do
 
       inside = mod(crossings, 2)
-      999   continue
+999   continue
       if (jins == 0) inside = 1 - inside
 
    end function pinpok_elemental
