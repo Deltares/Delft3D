59e86422422be14e5cf242ca7d972ca9a69930a6
Robin van Westrenen <robin.vanwestrenen@deltares.nl>
2025-12-05 15:26:20 +0100

    Extract artifacts from container run.

--- ci/teamcity/Delft3D/windows/test.kt
+++ ci/teamcity/Delft3D/windows/test.kt
@@ -26,13 +26,7 @@ object WindowsTest : BuildType({
     name = "Test"
     buildNumberPattern = "%product%: %build.vcs.number%"
 
-    artifactRules = """
-        test\deltares_testbench\data\cases\**\*.pdf      => pdf
-        test\deltares_testbench\data\cases\**\*.dia      => logging
-        test\deltares_testbench\data\cases\**\*.log      => logging
-        test\deltares_testbench\logs                     => logging
-        test\deltares_testbench\copy_cases               => copy_cases.zip
-    """.trimIndent()
+    artifactRules = "artifacts/**"
 
     val filePath = "${DslContext.baseDir}/vars/dimr_testbench_table.csv"
     val processor = CsvProcessor(filePath, "win64")
@@ -75,34 +69,71 @@ object WindowsTest : BuildType({
 
     steps {
         mergeTargetBranch {}
-        python {
-            name = "Run TestBench.py"
-            id = "RUNNER_testbench"
-            workingDir = "C:\\deltares_testbench"
-            command = file {
-                filename = "TestBench.py"
-                scriptArguments = """
-                    --username "%s3_dsctestbench_accesskey%"
-                    --password "%s3_dsctestbench_secret%"
-                    --compare
-                    --config "configs/%configfile%"
-                    --filter "testcase=%case_filter%"
-                    --log-level DEBUG
-                    --parallel
-                    --teamcity
-                """.trimIndent()
-            }
-            dockerImage = "containers.deltares.nl/delft3d-dev/test/delft3d-test-environment-windows:%container.tag%"
-            dockerImagePlatform = PythonBuildStep.ImagePlatform.Windows
-            dockerPull = true
-            dockerRunParameters = "-v %system.teamcity.build.checkoutDir%\\test\\deltares_testbench\\data\\engines:C:\\deltares_testbench\\data\\engines --memory %teamcity.agent.hardware.memorySizeMb%m --cpus %teamcity.agent.hardware.cpuCount%"
+        powerShell {
+            name = "Run TestBench.py in persistent container"
+            workingDir = "%system.teamcity.build.checkoutDir%"
+            scriptMode = BuildStep.ScriptMode.INLINE
+            script = """
+                \$container = "delft3d-test-%teamcity.build.id%"
+
+                docker run --name \$container `
+                    -v "%system.teamcity.build.checkoutDir%\\test\\deltares_testbench\\data\\engines:C:\\deltares_testbench\\data\\engines" `
+                    --memory %teamcity.agent.hardware.memorySizeMb%m `
+                    --cpus %teamcity.agent.hardware.cpuCount% `
+                    -e copy_cases="%copy_cases%" `
+                    containers.deltares.nl/delft3d-dev/test/delft3d-test-environment-windows:%container.tag% `
+                    powershell -Command ^
+                        "
+                        Set-Location C:\\deltares_testbench;
+                        python TestBench.py `
+                            --username '%s3_dsctestbench_accesskey%' `
+                            --password '%s3_dsctestbench_secret%' `
+                            --compare `
+                            --config 'configs/%configfile%' `
+                            --filter 'testcase=%case_filter%' `
+                            --log-level DEBUG `
+                            --parallel `
+                            --teamcity;
+                        \$testExit = \$LASTEXITCODE;
+
+                        Write-Host '##teamcity[blockOpened name=''Collecting selected artifacts inside container'']';
+                        \$inside = 'C:\\artifacts_inside';
+                        New-Item -Path \$inside -ItemType Directory -Force | Out-Null;
+
+                        robocopy 'data\\cases' '\$inside\\cases' *.diag *.log *.pdf /S /NJH /NJS /NP | Out-Null;
+                        if (Test-Path 'logs') { robocopy 'logs' '\$inside\\logs' /E /NJH /NJS /NP | Out-Null; }
+
+                        if ('%copy_cases%' -eq 'true') {
+                            Compress-Archive -Path 'data\\cases\\*' -DestinationPath '\$inside\\copy_cases.zip' -Force -CompressionLevel Optimal;
+                            \$size = \"{0:N1}\" -f ((Get-Item '\$inside\\copy_cases.zip').Length / 1MB);
+                            Write-Host \"copy_cases.zip created inside container (`$size MB)\";
+                        }
+
+                        Write-Host '##teamcity[blockClosed name=''Collecting selected artifacts inside container'']';
+
+                        exit \$testExit
+                        "
+            """
         }
-        script {
-            name = "Copy cases"
-            executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
-            conditions { equals("copy_cases", "true") }
-            workingDir = "test/deltares_testbench"
-            scriptContent = "xcopy \"data\\cases\" \"copy_cases\" /E /I /Y"
+        powerShell {
+            name = "Extract artifacts from stopped container"
+            executionMode = BuildStep.ExecutionMode.ALWAYS
+            scriptMode = BuildStep.ScriptMode.INLINE
+            script = """
+                \$container = "delft3d-test-%teamcity.build.id%"
+                \$target    = "%teamcity.build.checkoutDir%\\artifacts"
+
+                New-Item -Path \$target -ItemType Directory -Force | Out-Null
+                docker cp \"\$container`:C:/artifacts_inside/. \"\$target\\\"
+            """
+        }
+        powerShell {
+            name = "Cleanup container"
+            executionMode = BuildStep.ExecutionMode.ALWAYS
+            scriptMode = BuildStep.ScriptMode.INLINE
+            script = """
+                docker rm -f "delft3d-test-%teamcity.build.id%"
+            """
         }
     }
 
