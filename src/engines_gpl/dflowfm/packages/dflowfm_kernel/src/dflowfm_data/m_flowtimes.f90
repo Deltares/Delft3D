f240fa27db55df4d936cfa88095be6ad4af05359
Victor Chavarrias <victor.chavarrias@deltares.nl>
2026-01-23 16:19:13 +0100

    Revert "Correction to description of `dtsc`."
    This reverts commit 660f098be705e83141dee8a86feb96cc051f49a6.

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/m_flowtimes.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/m_flowtimes.f90
@@ -73,7 +73,7 @@ module m_flowtimes
    real(kind=dp) :: time_user !< Next time of external forcings update (steps increment by dt_user).
 
    real(kind=dp) :: dts !< internal computational timestep (s)
-   real(kind=dp) :: dtsc !< max timestep of limiting point kkcflmx (s)
+   real(kind=dp) :: dtsc !< max timstep of limiting point kkcflmx, zero if larger than dt_max
    real(kind=dp) :: dt_fac_max !< max dts increase factor
    real(kind=dp) :: dti !< inverse  computational timestep (1/s)
    real(kind=dp) :: dtprev !< previous computational timestep (s)  (1s is a bit like sobek)
