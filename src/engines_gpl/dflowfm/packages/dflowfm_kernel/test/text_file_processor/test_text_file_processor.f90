0a105d8a47806a1e49c0cde71c6301224dd1e70f
Mihai Pricope <mihai.pricope@deltares.nl>
2026-01-26 10:41:22 +0000

    basic wrapping over old properties module

---
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/test/text_file_processor/test_text_file_processor.f90
@@ -0,0 +1,96 @@
+!!  Copyright (C)  Stichting Deltares, 2012-2025.
+!!
+!!  This program is free software: you can redistribute it and/or modify
+!!  it under the terms of the GNU General Public License version 3,
+!!  as published by the Free Software Foundation.
+!!
+!!  This program is distributed in the hope that it will be useful,
+!!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+!!  GNU General Public License for more details.
+!!
+!!  You should have received a copy of the GNU General Public License
+!!  along with this program. If not, see <http://www.gnu.org/licenses/>.
+!!
+!!  contact: delft3d.support@deltares.nl
+!!  Stichting Deltares
+!!  P.O. Box 177
+!!  2600 MH Delft, The Netherlands
+!!
+!!  All indications and logos of, and references to registered trademarks
+!!  of Stichting Deltares remain the property of Stichting Deltares. All
+!!  rights reserved.
+module test_text_file_processor
+   use precision
+   use assertions_gtest
+
+   implicit none
+
+contains
+
+   !$f90tw TESTCODE(TEST, test_text_file_processor, test_notfound, test_notfound,
+   subroutine test_notfound() bind(C)
+      use messagehandling, only: LEVEL_INFO, LEVEL_WARN, LEVEL_ERROR, msgbuf, mess, msg_flush
+      use text_file_processor, only: TextFileProcessor
+      type(TextFileProcessor) :: processor
+
+      processor = TextFileProcessor('example.txt')
+      call processor%init()
+
+      call f90_assert_eq(processor%is_error, .true., 'Processor should indicate error for non-existing file.')
+
+      ! Check if msgbuf starts with "File does not exist"
+      call f90_assert_streq(msgbuf(1:19)//C_NULL_CHAR, 'File does not exist', 'msgbuf should start with "File does not exist".')
+      call msg_flush()
+
+   end subroutine
+   !$f90tw)
+
+   !$f90tw TESTCODE(TEST, test_text_file_processor, test_basic, test_basic,
+   subroutine test_basic() bind(C)
+      use messagehandling, only: LEVEL_INFO, LEVEL_WARN, LEVEL_ERROR, msgbuf, mess, msg_flush
+      use text_file_processor, only: TextFileProcessor
+      type(TextFileProcessor) :: processor
+
+      processor = TextFileProcessor('tt3.mdu')
+      call processor%init()
+      call msg_flush()
+
+      call f90_assert_eq(processor%is_error, .false., 'Processor should indicate no error for existing file.')
+
+      call processor%parse()
+
+      call msg_flush()
+
+   end subroutine
+   !$f90tw)
+
+   !$f90tw TESTCODE(TEST, test_text_file_processor, test_required_verifier, test_required_verifier,
+   subroutine test_required_verifier() bind(C)
+      use messagehandling, only: LEVEL_INFO, LEVEL_WARN, LEVEL_ERROR, msgbuf, mess, msg_flush
+      use text_file_processor, only: TextFileProcessor, ChapterPropsVerifier
+      type(TextFileProcessor) :: processor
+      type(ChapterPropsVerifier) :: verifier
+      character(len=:), allocatable :: required_strings(:)
+
+      required_strings = [ 'OutputDir', 'MapFormat' ]
+      verifier = ChapterPropsVerifier('output', required_strings)
+
+      processor = TextFileProcessor('tt3.mdu')
+      call processor%init()
+      call msg_flush()
+
+      call f90_assert_eq(processor%is_error, .false., 'Processor should indicate no error for existing file.')
+
+      call processor%parse()
+
+      ! Verify required strings
+      call f90_assert_eq(verifier%verify(processor), .true., 'All required strings should be present in the file.')
+      verifier = ChapterPropsVerifier('output', [ 'NonExistingProp' ])
+      call f90_assert_eq(verifier%verify(processor), .false., 'Verification should fail for missing required strings.')
+
+      call msg_flush()
+   end subroutine
+   !$f90tw)
+
+end module test_text_file_processor
---
+++ src/utils_lgpl/deltares_common/packages/deltares_common/src/text_file_processor.f90
@@ -0,0 +1,131 @@
+module text_file_processor
+   use messageHandling, only: warn_flush, err_flush, msgbuf, LEVEL_FATAL, LEVEL_ERROR, LEVEL_WARN, LEVEL_INFO
+   use properties
+   use tree_data_types
+
+   implicit none
+
+   type :: TextFileProcessor
+      character(len=:), allocatable :: filename
+      logical :: is_error = .false.
+      type(tree_data), pointer   :: tree
+   contains
+      procedure :: init => text_file_processor_init
+      procedure :: parse => text_file_processor_parse
+   end type TextFileProcessor
+
+   interface TextFileProcessor
+      module procedure :: text_file_processor_constructor
+   end interface TextFileProcessor
+
+   !> Abstract interface for verification
+   type, abstract :: TextFileProcessorVerifier
+   contains
+      procedure(verify_interface), deferred :: verify
+   end type TextFileProcessorVerifier
+
+   abstract interface
+      function verify_interface(this, processor) result(is_valid)
+         import TextFileProcessorVerifier, TextFileProcessor
+         class(TextFileProcessorVerifier), intent(in) :: this
+         type(TextFileProcessor), intent(in) :: processor
+         logical :: is_valid
+      end function verify_interface
+   end interface
+
+   !> String array verifier implementation
+   type, extends(TextFileProcessorVerifier) :: ChapterPropsVerifier
+      character(len=:), allocatable :: required_props(:)
+      character(len=:), allocatable :: chapter_name
+   contains
+      procedure :: verify => string_array_verifier_verify
+   end type ChapterPropsVerifier
+
+   interface ChapterPropsVerifier
+      module procedure :: string_array_verifier_constructor
+   end interface ChapterPropsVerifier
+
+contains
+
+   !> Constructor for TextFileProcessor
+   function text_file_processor_constructor(filename) result(processor)
+      character(len=*), intent(in) :: filename
+      type(TextFileProcessor) :: processor
+
+      processor%filename = filename
+   end function text_file_processor_constructor
+
+   !> Constructor for ChapterPropsVerifier
+   function string_array_verifier_constructor(chapter_name, strings) result(verifier)
+      character(len=*), intent(in) :: chapter_name
+      character(len=*), intent(in) :: strings(:)
+      type(ChapterPropsVerifier) :: verifier
+      integer :: n
+
+      ! Allocate from source to preserve deferred character length
+      allocate(verifier%required_props, source=strings)
+      verifier%chapter_name = chapter_name
+   end function string_array_verifier_constructor
+
+   !> Initialize method
+   subroutine text_file_processor_init(this)
+      class(TextFileProcessor), intent(inout) :: this
+      logical :: file_exists
+
+      ! Check if the file exists
+      inquire (file=this%filename, exist=file_exists)
+
+      if (.not. file_exists) then
+         write (msgbuf, '(a,a,a)') 'File does not exist: ', trim(this%filename), '.'
+         this%is_error = .true.
+      end if
+
+   end subroutine text_file_processor_init
+
+   !> Parse method
+   subroutine text_file_processor_parse(this)
+      class(TextFileProcessor), intent(inout) :: this
+      integer :: istat
+
+      call prop_inifile(this%filename, this%tree, istat)
+      ! Parse the file
+      ! Add parsing logic here
+      if (istat /= 0) then
+         write (msgbuf, '(a,a,a)') 'Error parsing file: ', trim(this%filename), '.'
+         this%is_error = .true.
+      end if
+
+   end subroutine text_file_processor_parse
+
+   !> String array verifier implementation
+   function string_array_verifier_verify(this, processor) result(is_valid)
+      class(ChapterPropsVerifier), intent(in) :: this
+      type(TextFileProcessor), intent(in) :: processor
+      logical :: is_valid
+      integer :: i
+
+      character(len=:), allocatable :: value
+      logical :: found
+
+      ! Check if processor is valid first
+      is_valid = .not. processor%is_error
+
+      ! If valid, check required strings
+      if (is_valid .and. allocated(this%required_props)) then
+         do i = 1, size(this%required_props)
+            ! Add your string verification logic here
+            call prop_get_alloc_string(processor%tree, this%chapter_name, this%required_props(i), value, found)
+            ! print *, 'Verifying presence of string: ', trim(this%required_props(i))
+            if (.not. found) then
+               write (msgbuf, '(a,a,a)') 'Missing required property: ', trim(this%required_props(i)), '.'
+               is_valid = .false.
+            else if (allocated(value)) then
+               DEALLOCATE(value)
+            end if
+            ! For now, just return true
+         end do
+      end if
+
+   end function string_array_verifier_verify
+
+end module text_file_processor
