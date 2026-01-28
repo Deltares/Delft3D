4dcd7508ae121887b86b2487e668d24662f0aeef
Arjen Markus <arjen.markus@deltares.nl>
2026-01-28 15:28:49 +0100

    Generalise the robust implementation of sedimentaiton and deposition
    The proof of concept was geared to two tiny model set-ups. This commit tries to make the idea general. For this several extra parameters are required in the process configuration.
    Special attention is needed for the estimate of the maximum flux - do we get all the signs and the indices right?

--- src/engines_gpl/waq/waq_process/sedim.f90
+++ src/engines_gpl/waq/waq_process/sedim.f90
@@ -66,9 +66,6 @@ contains
         use m_extract_waq_attribute
         USE BottomSet     !  Module with definition of the waterbottom segments
 
-        IMPLICIT REAL    (A-H, J-Z)
-        IMPLICIT INTEGER (I)
-
         REAL(kind = real_wp) :: process_space_real  (*), FL    (*)
         INTEGER(kind = int_wp) :: IPOINT(*), INCREM(*), num_cells, NOFLUX, &
                 IEXPNT(4, *), IKNMRK(*), num_exchanges_u_dir, num_exchanges_v_dir, num_exchanges_z_dir, num_exchanges_bottom_dir
@@ -77,35 +74,37 @@ contains
 
         REAL(kind = real_wp) :: PSEDMIN
 
-        real, parameter      :: seconds_per_day = 86400.0
+        real(kind = real_wp), parameter :: seconds_per_day = 86400.0
 
-        !
-        ! For now!
-        !
-        real :: mass_flux(num_cells), volume(num_cells), flow(num_exchanges_z_dir)
-
-        volume(:) = 5.0e3
-        volume(19:20) = 0.05e3
-        flow(:) = 0.0
+        integer :: ip1,  ip2,  ip3,  ip4,  ip5,  ip6,  ip7,  ip8,  ip9,  ip10, &
+                   ip11, ip12, ip13, ip14, ip15, ip16, ip17, ip18, ip19, ip20, ip21, &
+                   in1,  in2,  in3,  in4,  in5,  in6,  in7,  in8,  in9,  in10, &
+                   in11, in12, in13, in14, in15, in16, in17, in18, in19, in20, in21
+        integer :: ivan, inaar, ip, ipv, ipn, ipq, iq, iseg, iflux, ikmrkv, ikmrkn, iwa1, iwa2, ikmrk1, ikmrk2, ik, iwater
+        real(kind = real_wp) :: conc, zersed, vsed, tau, tcrsed, delt, psed, alpha, p, pmax, maxsed, potsed
 
 
-        IP1 = IPOINT(1)
-        IP2 = IPOINT(2)
-        IP3 = IPOINT(3)
-        IP4 = IPOINT(4)
-        IP5 = IPOINT(5)
-        IP6 = IPOINT(6)
-        IP7 = IPOINT(7)
-        IP8 = IPOINT(8)
-        IP9 = IPOINT(9)
-        IP10 = IPOINT(10)
-        IP11 = IPOINT(11)
-        IP12 = IPOINT(12)
-        IP13 = IPOINT(13)
-        IP14 = IPOINT(14)
-        IP15 = IPOINT(15)
-        IP16 = IPOINT(16)
-        IP17 = IPOINT(17)
+        IP1 = IPOINT(1)       ! Concentration inorganic matter
+        IP2 = IPOINT(2)       ! Zeroth-order flux -- at all useful?
+        IP3 = IPOINT(3)       ! Sedimentation velocity
+        IP4 = IPOINT(4)       ! Bottom shear stress
+        IP5 = IPOINT(5)       ! Critical shear stress for sedimentation
+        IP6 = IPOINT(6)       ! Depth of the segments
+        IP7 = IPOINT(7)       ! Time step (for limiting the deposition flux)
+        IP8 = IPOINT(8)       ! Minimum depth for sedimentation/resuspension -- obsolete!
+        IP9 = IPOINT(9)       ! Fraction going directly to layer S2
+        IP10 = IPOINT(10)     ! Fraction total inorganic matter (TIM) in layer S2
+        IP11 = IPOINT(11)     ! Maximum allowable fraction (TIM) in layer S2
+        IP12 = IPOINT(12)     ! Minimum sedimentation probability (force sedimentation) -- useful?
+        IP13 = IPOINT(13)     ! Volume of the segments
+        IP14 = IPOINT(14)     ! Workspace (input), used for computing maximum allowed flux
+        IP15 = IPOINT(15)     ! (exchange) sedimentation velocity per exchange
+        IP16 = IPOINT(16)     ! (exchange) Flow rate, used for estimating the influx
+        IP17 = IPOINT(17)     ! (output) Sedimentation probability (used for adsorbed substances)
+        IP18 = IPOINT(18)     ! (output) Sedimentation flux to layer S1
+        IP19 = IPOINT(19)     ! (output) Sedimentation flux to layer S2
+        IP20 = IPOINT(20)     ! Workspace (output), storing maximum allowed flux
+        IP21 = IPOINT(21)     ! (additional velocity) Sedimentation velocity
 
         IN1 = INCREM(1)
         IN2 = INCREM(2)
@@ -124,6 +123,10 @@ contains
         IN15 = INCREM(15)
         IN16 = INCREM(16)
         IN17 = INCREM(17)
+        IN18 = INCREM(18)
+        IN19 = INCREM(19)
+        IN20 = INCREM(20)
+        IN21 = INCREM(21)
 
         !
         ! Determine the mass influx to be expected for the cells near the bottom
@@ -131,14 +134,20 @@ contains
         ! Note: this does not work properly for floating material!
         !
         !
+
         do iseg = 1,num_cells
-            mass_flux(iseg) = 0.0  !! TODO: process_space_real
+            ip = ipoint(14) + (iseg - 1) * increm(14)
+            process_space_real(ip) = 0.0
         enddo
 
         DO IQ = 1, num_exchanges_u_dir + num_exchanges_v_dir + num_exchanges_z_dir
 
-            IVAN = IEXPNT(1, IQ)
+            IVAN  = IEXPNT(1, IQ)
             INAAR = IEXPNT(2, IQ)
+
+            ipv   = ipoint(14) + (ivan  - 1) * increm(14)
+            ipn   = ipoint(14) + (inaar - 1) * increm(14)
+            ipq   = ipoint(16) + (iq    - 1) * increm(16)
 
             IKMRKV = 1
             IKMRKN = 1
@@ -155,71 +164,73 @@ contains
                 ENDIF
             ENDIF
 
-            write(88,*) iq, ikmrkv, ikmrkn, flow(iq), ivan, inaar
-
+            !
+            ! Handle "from" segment
+            !
             IF ( (IKMRKV == 0 .OR. IKMRKV == 3 ) ) THEN
                 if ( inaar > 0 ) then
-                    ip1 = ipoint(1) + (ivan-1) * increm(1)
-                    mass_flux(ivan) = mass_flux(ivan) + max( 0.0, -flow(iq) * process_space_real(ip1) )
+                    ip1 = ipoint(1) + (inaar-1) * increm(1)
                 else
-                    !! Very specific for this test case!
-                    mass_flux(ivan) = mass_flux(ivan) + max( 0.0, -flow(iq) * 50.0 )
+                    ! Use the "to" segment as a proxy
+                    ip1 = ipoint(1) + (ivan-1) * increm(1)
                 endif
+
+                process_space_real(ipv) = process_space_real(ipv) + &
+                    max( 0.0, -process_space_real(ipq) * process_space_real(ip1) )
             ENDIF
+            !
+            ! Handle "to" segment
+            !
             IF ( (IKMRKN == 0 .OR. IKMRKN == 3 ) ) THEN
                 if ( ivan > 0 ) then
                     ip1 = ipoint(1) + (ivan-1) * increm(1)
-                    mass_flux(inaar) = mass_flux(inaar) + max( 0.0, flow(iq) * process_space_real(ip1) )
                 else
-                    !! Very specific for this test case!
-                    mass_flux(inaar) = mass_flux(inaar) + max( 0.0, flow(iq) * 50.0 )
+                    ip1 = ipoint(1) + (inaar-1) * increm(1)
                 endif
+
+                process_space_real(ipn) = process_space_real(ipn) + &
+                    max( 0.0, process_space_real(ipq) * process_space_real(ip1) )
             ENDIF
 
             ! If the exchange is a vertical exchange, also include the settling from the cell above
 
-            if ( iq > num_exchanges_u_dir + num_exchange_v_dir ) then
+            if ( iq > num_exchanges_u_dir + num_exchanges_v_dir ) then
                 if ( ivan > 0 ) then
                     ip1 = ipoint(1) + (ivan-1) * increm(1)
                     ip3 = ipoint(3) + (ivan-1) * increm(3)
-                    mass_flux(ivan) = mass_flux(ivan) + max( 0.0, -process_space_real(ip3) * process_space_real(ip1) ) / seconds_per_day
+                    process_space_real(ipv) = process_space_real(ipv) + &
+                        max( 0.0, -process_space_real(ip3) * process_space_real(ip1) ) / seconds_per_day
                 endif
 
                 if ( inaar > 0 ) then
                     ip1 = ipoint(1) + (inaar-1) * increm(1)
                     ip3 = ipoint(3) + (inaar-1) * increm(3)
-                    mass_flux(inaar) = mass_flux(inaar) + max( 0.0, process_space_real(ip3) * process_space_real(ip1) ) / seconds_per_day
+                    process_space_real(ipn) = process_space_real(ipn) + &
+                        max( 0.0, process_space_real(ip3) * process_space_real(ip1) ) / seconds_per_day
                 endif
             endif
-
-            if ( inaar > 0 ) then
-                write(88,*) iq, mass_flux(inaar), volume(inaar)
-            endif
-            if ( ivan > 0 ) then
-                write(88,*) iq, mass_flux(ivan), volume(ivan)
-            endif
-
         end do
 
         do iseg = 1,num_cells
-            if ( volume(iseg) > 0.0 ) then
-                mass_flux(iseg) = seconds_per_day * mass_flux(iseg) / volume(iseg)
+            ip  = ipoint(14) + (iseg - 1) * increm(14)
+            ipv = ipoint(13) + (iseg - 1) * increm(13)
+            if ( process_space_real(ipv) > 0.0 ) then
+                process_space_real(ip) = seconds_per_day * process_space_real(ip) / process_space_real(ipv)
             else
-                mass_flux(iseg) = 0.0
+                process_space_real(ip) = 0.0
             endif
         enddo
 
-        write(88,*) 'Mass flux: ', mass_flux(1)
-
-
         IFLUX = 0
+        IP1   = IPOINT(1)
+        IP3   = IPOINT(3)
         DO ISEG = 1, num_cells
 
             !     zero output
 
-            process_space_real (IP14) = 0.0
-            process_space_real (IP15) = 0.0
-            process_space_real (IP16) = 0.0
+            process_space_real (IP17) = 0.0
+            process_space_real (IP18) = 0.0
+            process_space_real (IP19) = 0.0
 
             !     sedimentation towards the bottom
 
@@ -275,7 +286,7 @@ contains
 
                         !        limit sedimentation to available mass (M/L2/DAY)
                         !!MAXSED = MIN (POTSED, CONC / DELT * DEPTH)
-                        MAXSED = MIN (POTSED, mass_flux(iseg) * DEPTH)
+                        MAXSED = MIN (POTSED, process_space_real(ip13) * DEPTH)
 
                         !        convert sedimentation to flux in M/L3/DAY
                         FL(1 + IFLUX) = MAXSED * (1. - ALPHA) / DEPTH
@@ -283,9 +294,9 @@ contains
                     ENDIF
 
                     !     Output of calculated sedimentation rate
-                    process_space_real (IP14) = PSED
-                    process_space_real (IP15) = MAXSED * (1. - ALPHA)
-                    process_space_real (IP16) = MAXSED * ALPHA
+                    process_space_real (IP17) = PSED
+                    process_space_real (IP18) = MAXSED * (1. - ALPHA)
+                    process_space_real (IP19) = MAXSED * ALPHA
                     !
                 ENDIF
             ENDIF
@@ -306,23 +317,26 @@ contains
             IP14 = IP14 + IN14
             IP15 = IP15 + IN15
             IP16 = IP16 + IN16
+            IP17 = IP17 + IN17
+            IP18 = IP18 + IN18
+            IP19 = IP19 + IN19
         end do
         !
         IP1 = IPOINT(1)
         IP6 = IPOINT(6)
         IP8 = IPOINT(8)
-        IP15 = IPOINT(15)
+        IP18 = IPOINT(18)
 
         !.....Exchangeloop over de horizontale richting
         DO IQ = 1, num_exchanges_u_dir + num_exchanges_v_dir
 
-            process_space_real(IP17) = 0.0
+            process_space_real(IP21) = 0.0
 
-            IP17 = IP17 + IN17
+            IP21 = IP21 + IN21
 
         end do
 
-        IP13 = IP13 + (num_exchanges_u_dir + num_exchanges_v_dir) * IN13
+        IP15 = IP15 + (num_exchanges_u_dir + num_exchanges_v_dir) * IN15
 
         !.....Exchangeloop over de verticale richting
         DO IQ = num_exchanges_u_dir + num_exchanges_v_dir + 1, num_exchanges_u_dir + num_exchanges_v_dir + num_exchanges_z_dir + num_exchanges_bottom_dir
@@ -359,18 +373,18 @@ contains
                     MINDEP = process_space_real(IP8 + (IVAN - 1) * IN8)
                     MINDE2 = process_space_real(IP8 + (INAAR - 1) * IN8)
                     IF (DEPTH > MINDEP .AND. DEPTH2 > MINDE2) THEN
-                        process_space_real(IP17) = process_space_real(IP13) / 86400.
+                        process_space_real(IP21) = process_space_real(IP15) / seconds_per_day
                     ELSE
-                        process_space_real(IP17) = 0.0
+                        process_space_real(IP21) = 0.0
                     ENDIF
                 ELSE
-                    process_space_real(IP17) = 0.0
+                    process_space_real(IP21) = 0.0
                 ENDIF
 
             ENDIF
 
-            IP13 = IP13 + IN13
-            IP17 = IP17 + IN17
+            IP15 = IP15 + IN15
+            IP21 = IP21 + IN21
 
         end do
 
@@ -393,6 +407,11 @@ contains
         IP15 = IPOINT(15)
         IP16 = IPOINT(16)
         IP17 = IPOINT(17)
+        IP17 = IPOINT(17)
+        IP18 = IPOINT(18)
+        IP19 = IPOINT(19)
+        IP20 = IPOINT(20)
+        IP21 = IPOINT(21)
 
         DO IK = 1, Coll%current_size
 
@@ -436,7 +455,7 @@ contains
                 ENDIF
 
                 IF (CONC > 1.E-10) THEN
-                    process_space_real(IP17 + (IQ - 1) * IN17) = MAXSED / 86400. / CONC
+                    process_space_real(IP21 + (IQ - 1) * IN21) = MAXSED / 86400. / CONC
                 ENDIF
 
             ENDDO
