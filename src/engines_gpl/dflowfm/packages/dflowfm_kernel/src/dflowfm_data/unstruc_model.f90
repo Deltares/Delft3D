9c730acce4f35fb58d50a5ce6649fdaa6e76df13
thomas-pijls <thomas.pijls@deltares.nl>
2026-01-29 12:02:14 +0100

    UNST-4638: Review comment

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/unstruc_model.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/unstruc_model.f90
@@ -938,8 +938,10 @@ contains
             numtopsig = 0
          end if
          call prop_get(md_ptr, 'geometry', 'Numtopsiguniform', JaNumtopsiguniform)
-         call prop_get(md_ptr, 'geometry', 'sigmaGrowthFactor', z_layer_growth_factor)
-         call prop_get(md_ptr, 'geometry', 'zLayerGrowthFactor', z_layer_growth_factor)
+         call prop_get(md_ptr, 'geometry', 'sigmaGrowthFactor', z_layer_growth_factor, success) ! Deprecated, sigmaGrowthFactor is replaced by zLayerGrowthFactor
+         if (.not. success) then
+            call prop_get(md_ptr, 'geometry', 'zLayerGrowthFactor', z_layer_growth_factor)
+         end if
          call prop_get(md_ptr, 'geometry', 'Dztopuniabovez', dztopuniabovez)
          call prop_get(md_ptr, 'geometry', 'Dztop', Dztop)
          call prop_get(md_ptr, 'geometry', 'Toplayminthick', Toplayminthick)
