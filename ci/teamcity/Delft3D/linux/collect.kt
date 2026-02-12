26a45609700c0ffb5e04ade56bd44c46e97939a0
Bart Steur <bart.steur@deltares.nl>
2026-02-12 13:33:40 +0100

    add retention parameter linux

--- ci/teamcity/Delft3D/linux/collect.kt
+++ ci/teamcity/Delft3D/linux/collect.kt
@@ -78,7 +78,8 @@ object LinuxCollect : BuildType({
             param("nexus_password", "%nexus_password%")
             param("nexus_repo", "/delft3d-dev")
             param("nexus_url", "https://artifacts.deltares.nl/repository")
-            param("target_path", "/07_day_retention/dimrset/%file_path%")
+            param("retention_period", "07_day_retention")
+            param("target_path", "/dimrset/%file_path%")
         }
     }
 
