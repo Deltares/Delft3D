2ae477f69b4b693886f514016f444fdc31131233
Robin van Westrenen <robin.vanwestrenen@deltares.nl>
2026-02-20 14:59:03 +0100

    Allow Linux tests to run on non-FIPS agents too.

--- ci/teamcity/Delft3D/linux/test.kt
+++ ci/teamcity/Delft3D/linux/test.kt
@@ -17,7 +17,7 @@ object LinuxTest : BuildType({
     description = "Run TestBench.py within the Docker container on a list of testbench XML files."
 
     templates(
-        TemplateLinuxAgentFips,
+        TemplateLinuxAgent,
         TemplateMergeRequest,
         TemplatePublishStatus,
         TemplateMonitorPerformance,
