2159bfa95470aea5fe959da43f82aa7c1fcb1a98
Harmen Wierenga <harmen.wierenga@deltares.nl>
2026-02-27 14:37:36 +0100

    Add imported lib properties precice to work in debug, Update C++ style csumo_precice

--- src/tools_gpl/csumo_precice/src/main.cpp
+++ src/tools_gpl/csumo_precice/src/main.cpp
@@ -1,20 +1,23 @@
-#include "csumo_precice_lib.hpp"
-#include <iostream>
 #include <cstdlib>
+#include <print>
+#include <string_view>
 
-int main(int argc, char** argv) 
+#include "csumo_precice_lib.hpp"
+
+int main(int argc, char** argv)
 {
-    if (argc != 3) {
-        std::cout << "The csumo_precice solver was called with an incorrect number of arguments.\n";
-        std::cout << "Usage: ./csumo_precice configFile solverName\n\n";
-        std::cout << "Parameter description\n";
-        std::cout << "  configFile: Path and filename of preCICE configuration\n";
-        std::cout << "  solverName: Participant name in preCICE configuration\n";
+    if (argc != 3)
+    {
+        std::println("The csumo_precice solver was called with an incorrect number of arguments.");
+        std::println("Usage: ./csumo_precice configFile solverName\n");
+        std::println("Parameter description");
+        std::println("  configFile: Path and filename of preCICE configuration");
+        std::println("  solverName: Participant name in preCICE configuration");
         return EXIT_FAILURE;
     }
 
-    std::string configFileName(argv[1]);
-    std::string solverName(argv[2]);
+    const std::string_view configFileName(argv[1]);
+    const std::string_view solverName(argv[2]);
 
     return csumo_precice::csumo_precice(configFileName, solverName);
 }
