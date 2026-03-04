0d0a913c8db4abd227225a6dbee13179624a4fc5
Harmen Wierenga <harmen.wierenga@deltares.nl>
2026-03-04 10:54:52 +0100

    Clean up csumo_precice, add boost program options lib

--- src/tools_gpl/csumo_precice/src/main.cpp
+++ src/tools_gpl/csumo_precice/src/main.cpp
@@ -1,23 +1,61 @@
+#include <boost/program_options.hpp>
 #include <cstdlib>
 #include <print>
+#include <sstream>
+#include <string>
 #include <string_view>
 
 #include "csumo_precice_lib.hpp"
 
+namespace po = boost::program_options;
+
 int main(int argc, char** argv)
 {
-    if (argc != 3)
+    std::string configFileName;
+    std::string solverName;
+
+    boost::program_options::options_description description("csumo_precice options");
+    // clang-format off
+    description.add_options()
+        ("help,h",
+            "Show this help message")
+        ("config-file,c", boost::program_options::value<std::string>(&configFileName)->required(),
+            "Path and filename of preCICE configuration")
+        ("solver-name,s", boost::program_options::value<std::string>(&solverName)->required(),
+            "Participant name in preCICE configuration");
+    // clang-format on
+
+    boost::program_options::positional_options_description positionals;
+    positionals.add("config-file", 1);
+    positionals.add("solver-name", 1);
+
+    const auto usage = [&description] {
+        std::ostringstream oss;
+        oss << "Usage: csumo_precice [options] configFile solverName\n\n" << description << '\n';
+        return oss.str();
+    };
+
+    try
     {
-        std::println("The csumo_precice solver was called with an incorrect number of arguments.");
-        std::println("Usage: ./csumo_precice configFile solverName\n");
-        std::println("Parameter description");
-        std::println("  configFile: Path and filename of preCICE configuration");
-        std::println("  solverName: Participant name in preCICE configuration");
+        boost::program_options::variables_map vm;
+        boost::program_options::store(
+            boost::program_options::command_line_parser(argc, argv).options(description).positional(positionals).run(),
+            vm);
+
+        if (vm.count("help"))
+        {
+            std::print("{}", usage());
+            return EXIT_SUCCESS;
+        }
+
+        boost::program_options::notify(vm);
+    }
+    catch (const boost::program_options::error& e)
+    {
+        std::println(stderr, "Error: {}\n", e.what());
+        std::print(stderr, "{}", usage());
         return EXIT_FAILURE;
     }
-
-    const std::string_view configFileName(argv[1]);
-    const std::string_view solverName(argv[2]);
 
     return csumo_precice::csumo_precice(configFileName, solverName);
 }
