27177961822e97bd90f7401e6c9e8d7433057cba
FlorisBuwaldaDeltares <floris.buwalda@Deltares.nl>
2026-03-06 18:14:57 +0100

    proper interface for tricall

--- src/utils_lgpl/ec_module/packages/ec_module/src/ec_basic_interpolation.f90
+++ src/utils_lgpl/ec_module/packages/ec_module/src/ec_basic_interpolation.f90
@@ -147,8 +147,33 @@ module m_ec_basic_interpolation
    public :: triinterp2
    public :: TerrorInfo
    public :: interpolate_linear_from_triangle
+   public :: tricall
+
+#if defined(_WIN32) || defined(WIN32)
+#  define TRICALL_SYMBOL 'TRICALL'
+#else
+#  define TRICALL_SYMBOL 'tricall_'
+#endif
+      interface
+         subroutine TRICALL(jatri, xs, ys, ns, indx, numtri, edgeidx, numedge, triedge, xs3, ys3, ns3, trisize) &
+            bind(C, name=TRICALL_SYMBOL)
+            use iso_c_binding
+            integer(c_int), intent(in) :: jatri ! C reads *jatri only, never writes back
+            real(c_double), intent(in) :: xs(*), ys(*)
+            integer(c_int), intent(in) :: ns
+            integer(c_int), intent(inout) :: indx(*)
+            integer(c_int), intent(inout) :: numtri
+            integer(c_int), intent(inout) :: edgeidx(*)
+            integer(c_int), intent(inout) :: numedge
+            integer(c_int), intent(inout) :: triedge(*)
+            real(c_double), intent(inout) :: xs3(*), ys3(*)
+            integer(c_int), intent(inout) :: ns3
+            real(c_double), intent(inout) :: trisize
+         end subroutine TRICALL
+      end interface
 
 contains
+
 
    !> Bilinear interpolation for uniform rectangular.
    !! TODO: move to ec_basic_interpolation or bilin5
@@ -720,24 +745,6 @@ contains
       real(kind=hp) :: trisize
       real(kind=hp) :: XH(NH), YH(NH)
       integer, allocatable :: idum(:)
-
-      interface
-         subroutine TRICALL(jatri, xs, ys, ns, indx, numtri, edgeidx, numedge, triedge, xs3, ys3, ns3, trisize) &
-            bind(C, name='TRICALL')
-            use iso_c_binding
-            integer(c_int), intent(in) :: jatri ! C reads *jatri only, never writes back
-            real(c_double), intent(in) :: xs(*), ys(*)
-            integer(c_int), intent(in) :: ns
-            integer(c_int), intent(inout) :: indx(*)
-            integer(c_int), intent(inout) :: numtri
-            integer(c_int), intent(inout) :: edgeidx(*)
-            integer(c_int), intent(inout) :: numedge
-            integer(c_int), intent(inout) :: triedge(*)
-            real(c_double), intent(inout) :: xs3(*), ys3(*)
-            integer(c_int), intent(inout) :: ns3
-            real(c_double), intent(inout) :: trisize
-         end subroutine TRICALL
-      end interface
 
       !     check memory
       allocate (idum(50 * Ns), stat=ierr) ! probably not enough
