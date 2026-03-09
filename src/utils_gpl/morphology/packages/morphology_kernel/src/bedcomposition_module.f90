516e01bb87f8eaa3aeb67fe9e7de7d078b7f1e20
Victor Chavarrias <victor.chavarrias@deltares.nl>
2026-03-09 09:21:13 +0100

    Remove double deallocation.

--- src/utils_gpl/morphology/packages/morphology_kernel/src/bedcomposition_module.f90
+++ src/utils_gpl/morphology/packages/morphology_kernel/src/bedcomposition_module.f90
@@ -3040,7 +3040,6 @@ function clrmorlyr(this) result (istat)
        if (associated(state%msed))         deallocate(state%msed    , STAT = istat)
        if (associated(state%thlyr))        deallocate(state%thlyr   , STAT = istat)
        if (associated(state%sedshort))     deallocate(state%sedshort, STAT = istat)
-       if (associated(state%mobile))  deallocate(state%mobile, STAT = istat)
        !
        deallocate(this%state, STAT = istat)
        nullify(this%state)
