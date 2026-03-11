2ff4ee22bb9054aa366b44cc83e2d84a3d0060b1
Robin van Westrenen <robin.vanwestrenen@deltares.nl>
2026-03-11 15:43:41 +0100

    .

--- ci/teamcity/Delft3D/template/downloadFromDVC.kt
+++ ci/teamcity/Delft3D/template/downloadFromDVC.kt
@@ -55,30 +55,38 @@ object TemplateDownloadFromDVC : Template({
 
                 pushd "%%BASE_PATH%%"
 
-                echo [INFO] Pulling root doc/ first...
+                echo [INFO] 1/2 Pulling root doc.dvc ...
                 "%%DVC_EXE%%" pull doc.dvc
 
-                echo [INFO] Pulling f[0-9]* features in batches of 100 (low memory)...
+                echo [INFO] 2/2 Pulling all other f[0-9]* doc.dvc files in batches of 100 (to reduce memory usage)...
+
                 setlocal EnableDelayedExpansion
                 set "BATCH="
                 set "COUNT=0"
+                set "TOTAL=0"
 
-                for /f "delims=" %%%%a in ('dir /s /b f[0-9]*\\doc.dvc 2^>nul') do (
-                    set /a COUNT+=1
-                    set "BATCH=!BATCH! "%%%%a""
+                for /f "delims=" %%%%a in ('dir /s /b doc.dvc 2^>nul') do (
+                    echo "%%%%a" | findstr /i "\\f[0-9]" >nul
+                    if not errorlevel 1 (
+                        set /a TOTAL+=1
+                        set /a COUNT+=1
+                        set "BATCH=!BATCH! "%%%%a""
 
-                    if !COUNT! equ 100 (
-                        echo [BATCH] Pulling next 100 files...
-                        "%%DVC_EXE%%" pull !BATCH!
-                        set "BATCH="
-                        set "COUNT=0"
+                        if !COUNT! equ 100 (
+                            echo [BATCH] Pulling next 100 files (total so far: !TOTAL!)...
+                            "%%DVC_EXE%%" pull !BATCH!
+                            set "BATCH="
+                            set "COUNT=0"
+                        )
                     )
                 )
 
                 if not "!BATCH!"=="" (
-                    echo [BATCH] Pulling remaining files...
+                    echo [BATCH] Pulling remaining !COUNT! files (total: !TOTAL!)...
                     "%%DVC_EXE%%" pull !BATCH!
                 )
+
+                echo [INFO] Total f[0-9]* doc.dvc files processed: !TOTAL!
 
                 endlocal
                 popd
