e58b999825ee829ceffe8be7bac0ac0776b47a8f
DIRECTORY\groenenb <Julien.Groenenboom@deltares.nl>
2026-03-18 21:59:10 +0100

    Run fprettify

--- src/utils_lgpl/deltares_common/packages/deltares_common/src/geometry_module.f90
+++ src/utils_lgpl/deltares_common/packages/deltares_common/src/geometry_module.f90
@@ -338,7 +338,7 @@ contains
             if (xl < x_intersect) then
                ! ray crosses edge to the right of point
                crossings = crossings + 1
-            else if (equal(xl, x_intersect, TOLERANCE_FACTOR*epsilon(xl))) then
+            else if (equal(xl, x_intersect, TOLERANCE_FACTOR * epsilon(xl))) then
                ! point is exactly on the edge
                is_inside = .true.
                if (jins == 0) then
