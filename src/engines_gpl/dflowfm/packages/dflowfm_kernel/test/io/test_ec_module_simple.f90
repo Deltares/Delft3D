ea18f2ae3a49c18f24f4f46d70f4af17e6358aee
Mihai Pricope <mihai.pricope@deltares.nl>
2026-03-20 09:32:45 +0000

    some working test

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/test/io/test_ec_module_simple.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/test/io/test_ec_module_simple.f90
@@ -2,6 +2,8 @@ module test_ec_module_simple
    use precision
    use assertions_gtest
    use cwd
+   use fm_external_forcings, only: adduniformtimerelation_objects
+   use m_meteo, only: initialize_ec_module, ecInstancePtr, item_lateraldischarge, ec_gettimespacevalue
 
    implicit none
 
@@ -10,11 +12,35 @@ contains
    subroutine test_ec_simple() bind(C)
       character(len=1024) :: current_directory
       integer :: status
+      logical :: success
+      real(dp), dimension(3) :: test_array = [1.0_dp, 2.0_dp, 3.0_dp]
 
       status = getCWD(current_directory)
 
       print *, "Current working directory: ", trim(current_directory)
       print *, "Status: ", status
+      call initialize_ec_module()
+
+      success = adduniformtimerelation_objects('lateral_discharge', '', 'lateral', '9', 'discharge', 'ec_module/FlowFM_lateral_sources.bc', 1, &
+                                               1, test_array)
+
+      block
+         integer :: i
+         real(dp), dimension(:), allocatable :: indices, results
+         indices  = [5.0_dp, 60.0_dp, 65.0_dp, 105.0_dp, 155.0_dp, 300.0_dp]
+         allocate(results(size(indices)))
+         
+         do i = 1, size(indices)
+            success = ec_gettimespacevalue(ecInstancePtr, item_lateraldischarge, 20000101, 0.0_dp, 1, indices(i))
+            results(i) = test_array(1)
+         end do
+
+          print *, "indices    results"
+          do i = 1, size(indices)
+            print '(F10.1, F12.1)', indices(i), results(i)
+          end do
+      end block
+
    end subroutine test_ec_simple
    !$f90tw)
 
