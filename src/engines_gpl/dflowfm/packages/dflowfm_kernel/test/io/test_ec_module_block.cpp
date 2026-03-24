39a148d4ea12c1cfca1b32d7f9986568171f9221
Mihai Pricope <mihai.pricope@deltares.nl>
2026-03-24 09:00:25 +0000

    fix build

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/test/io/test_ec_module_block.cpp
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/test/io/test_ec_module_block.cpp
@@ -1,4 +1,4 @@
 /* set the fpp where the tests are implemented using f90tw "directives" in the fortran. */
-#define CURRENTTESTFILE "test_ec_module_simple.f90.h"
+#define CURRENTTESTFILE "test_ec_module_block.f90.h"
 
 #include "f90tw_gtest.h"
