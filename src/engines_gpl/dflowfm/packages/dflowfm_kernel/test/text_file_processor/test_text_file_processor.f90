72827edc3901e35cc36da1b99f8986c705e3d211
Mihai Pricope <mihai.pricope@deltares.nl>
2026-03-25 12:20:43 +0000

    remove IO need

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/test/text_file_processor/test_text_file_processor.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/test/text_file_processor/test_text_file_processor.f90
@@ -25,6 +25,7 @@ module test_text_file_processor
    use assertions_gtest
    use m_text_file_processor, only: TextFileProcessor
    use messagehandling, only: LEVEL_INFO, LEVEL_WARN, LEVEL_ERROR, msgbuf, mess, msg_flush
+   use m_file_helpers, only: create_file
 
    implicit none
 
@@ -49,7 +50,8 @@ contains
    subroutine test_basic() bind(C)
       type(TextFileProcessor) :: processor
 
-      processor = TextFileProcessor('tt3.mdu')
+      call create_file('test.mdu', [''])
+      processor = TextFileProcessor('test.mdu')
       call msg_flush()
 
       call f90_assert_eq(processor%is_error, .false., 'Processor should indicate no error for existing file.')
@@ -66,26 +68,59 @@ contains
       type(ChapterPropsVerifier) :: verifier
       character(len=:), allocatable :: required_strings(:)
 
-      required_strings = [ 'OutputDir', 'MapFormat' ]
+      call create_file('test.mdu', [ &
+                       "[external forcing]", &
+                       "ExtForceFileNew                      = tt3-new.ext", &
+                       "", &
+                       "[output]", &
+                       "OutputDir                            = dflowfmoutput", &
+                       "FlowGeomFile                         =", &
+                       "ObsFile                              = tt3_obs.xyn", &
+                       "CrsFile                              = tt3_crs.pli"])
+
+      required_strings = ['OutputDir', 'ObsFile']
       verifier = ChapterPropsVerifier('output', required_strings)
 
-      processor = TextFileProcessor('tt3.mdu')
+      processor = TextFileProcessor('test.mdu')
       call msg_flush()
 
       call f90_assert_eq(processor%is_error, .false., 'Processor should indicate no error for existing file.')
 
       ! Verify required strings
       call f90_assert_eq(verifier%verify(processor), .true., 'All required strings should be present in the file.')
-      verifier = ChapterPropsVerifier('output', [ 'NonExistingProp' ])
+      verifier = ChapterPropsVerifier('output', ['NonExistingProp'])
       call f90_assert_eq(verifier%verify(processor), .false., 'Verification should fail for missing required strings.')
 
       call msg_flush()
    end subroutine
    !$f90tw)
 
+   subroutine setup_ext_file()
+      call create_file('sorsin3D.ext', [ &
+                       "[BlockXYBad]", &
+                       "name             = discharge_salinity_temperature_sorsin", &
+                       "numCoordinates   = 3", &
+                       "xCoordinates     = 63.350456 45.200344 45.200344", &
+                       "yCoordinates     = 12.950216 6.350155", &
+                       "xCoordinates = 1.0, 2.0, 3.0", &
+                       "[BlockXYMissing]", &
+                       "name             = discharge_salinity_temperature_sorsin", &
+                       "numCoordinates   = 3", &
+                       "xCoordinates     = 63.350456 45.200344 45.200344", &
+                       "discharge    = leftsor.bc", &
+                       "salinityDelta    = leftsor.bc", &
+                       "", &
+                       "[BlockXYGood]", &
+                       "name             = discharge_salinity_temperature_sorsin", &
+                       "numCoordinates   = 2", &
+                       "xCoordinates     = 63.350456 45.200344", &
+                       "yCoordinates     = 12.950216 6.350155", &
+                       "xCoordinates = 1.0, 2.0, 3.0"])
+   end subroutine setup_ext_file
+
    !$f90tw TESTCODE(TEST, test_text_file_processor, test_and_verifier, test_and_verifier,
    subroutine test_and_verifier() bind(C)
-      use m_text_file_validators, only: ChapterPropsVerifier, AndVerifier, ArraysLengthVerifier, TextFileProcessorVerifier, VerifierPtr
+   use m_text_file_validators, only: ChapterPropsVerifier, AndVerifier, ArraysLengthVerifier, TextFileProcessorVerifier, VerifierPtr
 
       type(TextFileProcessor) :: processor
       class(TextFileProcessorVerifier), allocatable :: verifier1, verifier2
@@ -93,25 +128,25 @@ contains
       character(len=:), allocatable :: required_strings1(:), required_strings2(:)
       class(VerifierPtr), DIMENSION(:), allocatable :: verifiers
 
-      required_strings1 = [ 'discharge', 'salinityDelta' ]
+      call setup_ext_file()
+      required_strings1 = ['discharge', 'salinityDelta']
       verifier1 = ChapterPropsVerifier('BlockXYMissing', required_strings1)
-      verifier2  = ArraysLengthVerifier('BlockXYGood', [ 'xCoordinates', 'yCoordinates' ])
+      verifier2 = ArraysLengthVerifier('BlockXYGood', ['xCoordinates', 'yCoordinates'])
 
-      processor = TextFileProcessor('sorsin3D-new.ext')
+      processor = TextFileProcessor('sorsin3D.ext')
       call msg_flush()
 
       call f90_assert_eq(processor%is_error, .false., 'Processor should indicate no error for existing file.')
 
-
       ! Test AndVerifier with all conditions passing
-      verifiers = [ VerifierPtr(verifier1), VerifierPtr(verifier2) ]
+      verifiers = [VerifierPtr(verifier1), VerifierPtr(verifier2)]
       and_verifier = AndVerifier(verifiers)
       call f90_assert_eq(and_verifier%verify(processor), .true., 'AndVerifier should pass when all sub-verifiers pass.')
       call msg_flush()
 
       ! Test AndVerifier with one condition failing
-      verifier1 = ChapterPropsVerifier('BlockXYGood', [ 'NonExistentProperty' ])
-      verifiers = [ VerifierPtr(verifier1), VerifierPtr(verifier2) ]
+      verifier1 = ChapterPropsVerifier('BlockXYGood', ['NonExistentProperty'])
+      verifiers = [VerifierPtr(verifier1), VerifierPtr(verifier2)]
       and_verifier = AndVerifier(verifiers)
       call f90_assert_eq(and_verifier%verify(processor), .false., 'AndVerifier should fail when any sub-verifier fails.')
 
@@ -124,27 +159,27 @@ contains
 
       use m_text_file_validators, only: TextFileProcessorVerifier, ArraysLengthVerifier
 
-
       type(TextFileProcessor) :: processor
       class(TextFileProcessorVerifier), allocatable :: verifier
       character(len=:), allocatable :: required_strings(:)
       character(len=1024) :: cwd_string
 
-      processor = TextFileProcessor('sorsin3D-new.ext')
+      call setup_ext_file()
+      processor = TextFileProcessor('sorsin3D.ext')
       call msg_flush()
 
       call f90_assert_eq(processor%is_error, .false., 'Processor should indicate no error for existing file.')
 
-      verifier  = ArraysLengthVerifier('BlockXYBad', [ 'xCoordinates', 'yCoordinates' ])
+      verifier = ArraysLengthVerifier('BlockXYBad', ['xCoordinates', 'yCoordinates'])
       call f90_assert_eq(verifier%verify(processor), .false., 'BlockXYBad should not pass the verification as xCoordinates and yCoordinates have different lengths.')
 
-      verifier  = ArraysLengthVerifier('BlockXYGood', [ 'xCoordinates', 'yCoordinates' ])
+      verifier = ArraysLengthVerifier('BlockXYGood', ['xCoordinates', 'yCoordinates'])
       call f90_assert_eq(verifier%verify(processor), .true., 'BlockXYGood should pass the verification as xCoordinates and yCoordinates have the same length.')
 
       call msg_flush()
    end subroutine
    !$f90tw)
-   
+
    !$f90tw TESTCODE(TEST, test_text_file_processor, test_block_verifier, test_block_verifier,
    subroutine test_block_verifier() bind(C)
       use properties, only: prop_inifile
@@ -152,7 +187,6 @@ contains
       use tree_structures, only: tree_num_nodes, tree_get_name
       use m_text_file_validators, only: ChapterVerifier, ArraysLengthChapterVerifier
       use string_module, only: str_tolower
-
 
       type(TextFileProcessor) :: processor, tmpProcessor
       class(ChapterVerifier), allocatable :: verifier
@@ -163,24 +197,23 @@ contains
       type(tree_data), pointer :: block_ptr
       character(len=:), allocatable :: group_name
 
-      processor = TextFileProcessor('sorsin3D-new.ext')
+      call setup_ext_file()
+      processor = TextFileProcessor('sorsin3D.ext')
       call f90_assert_eq(processor%is_error, .false., 'Processor should indicate no error for existing file.')
 
-      ! call prop_inifile('sorsin3D-new.ext', tree, istat)
-      ! call f90_assert_eq(istat, 0, 'File should be parsed successfully.')
       num_items_in_file = tree_num_nodes(processor%tree)
       do i = 1, num_items_in_file
          block_ptr => processor%tree%child_nodes(i)%node_ptr
          group_name = trim(tree_get_name(block_ptr))
          if (trim(adjustl(str_tolower(group_name))) == trim(adjustl(str_tolower('BlockXYBad')))) then
             tmpProcessor = TextFileProcessor(block_ptr)
-            verifier  = ArraysLengthChapterVerifier([ 'xCoordinates', 'yCoordinates' ], 'numCoordinates')
+            verifier = ArraysLengthChapterVerifier(['xCoordinates', 'yCoordinates'], 'numCoordinates')
             call f90_assert_eq(verifier%verify(block_ptr), .false., 'BlockXYBad should not pass the verification as xCoordinates and yCoordinates have different lengths.')
          end if
 
          if (trim(adjustl(str_tolower(group_name))) == trim(adjustl(str_tolower('BlockXYGood')))) then
             tmpProcessor = TextFileProcessor(block_ptr)
-            verifier  = ArraysLengthChapterVerifier([ 'xCoordinates', 'yCoordinates' ], 'numCoordinates')
+            verifier = ArraysLengthChapterVerifier(['xCoordinates', 'yCoordinates'], 'numCoordinates')
             call f90_assert_eq(verifier%verify(block_ptr), .true., 'BlockXYGood should pass the verification as xCoordinates and yCoordinates have the same length.')
          end if
 
@@ -188,6 +221,5 @@ contains
 
    end subroutine
    !$f90tw)
-   
 
 end module test_text_file_processor
