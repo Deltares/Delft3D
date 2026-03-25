2261736ee4a741eacfebb3a603b357a76abcdb6d
Mihai Pricope <mihai.pricope@deltares.nl>
2026-03-25 09:40:48 +0000

    inline bc file

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/test/io/test_ec_module_block.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/test/io/test_ec_module_block.f90
@@ -3,9 +3,11 @@ module test_ec_module_block
    use assertions_gtest
    use fm_external_forcings, only: adduniformtimerelation_objects
    use m_meteo, only: initialize_ec_module, ecInstancePtr, item_lateraldischarge, ec_gettimespacevalue
+   use m_file_helpers, only: create_file
 
    implicit none
 
+   character(len=*), parameter :: BC_FILENAME = "test.bc"
 contains
    !$f90tw TESTCODE(TEST, test_ec_module_block, test_ec_block_from, test_ec_block_from,
    subroutine test_ec_block_from() bind(C)
@@ -14,7 +16,27 @@ contains
 
       call initialize_ec_module()
 
-      success = adduniformtimerelation_objects('lateral_discharge', '', 'lateral', '9', 'discharge', 'ec_module/FlowFM_lateral_sources.bc', 1, &
+      call create_file(BC_FILENAME, [ &
+                       "[General]", &
+                       "    fileVersion           = 1.01", &
+                       "    fileType              = boundConds", &
+                       "", &
+                       "[forcing]", &
+                       "    name                  = 9", &
+                       "    function              = timeseries", &
+                       "    timeInterpolation     = block-from", &
+                       "    quantity              = time", &
+                       "    unit                  = seconds since 2000-01-01 00:00:00", &
+                       "    quantity              = lateral_discharge", &
+                       "    unit                  = m³/s", &
+                       "    0 0", &
+                       "    10 100", &
+                       "    20 20", &
+                       "    30 300", &
+                       "    40 0"])
+
+      
+      success = adduniformtimerelation_objects('lateral_discharge', '', 'lateral', '9', 'discharge', BC_FILENAME, 1, &
                                                1, test_array)
 
       block
@@ -41,9 +63,28 @@ contains
       logical :: success
       real(dp), dimension(3) :: test_array = [1.0_dp, 2.0_dp, 3.0_dp]
 
+      call create_file(BC_FILENAME, [ &
+                       "[General]", &
+                       "    fileVersion           = 1.01", &
+                       "    fileType              = boundConds", &
+                       "", &
+                       "[forcing]", &
+                       "    name                  = 9", &
+                       "    function              = timeseries", &
+                       "    timeInterpolation     = block-from", &
+                       "    quantity              = time", &
+                       "    unit                  = seconds since 2000-01-01 00:00:00", &
+                       "    quantity              = lateral_discharge", &
+                       "    unit                  = m³/s", &
+                       "    0 0", &
+                       "    10 100", &
+                       "    20 20", &
+                       "    30 300", &
+                       "    40 0"])
+
       call initialize_ec_module()
 
-      success = adduniformtimerelation_objects('lateral_discharge', '', 'lateral', '9', 'discharge', 'ec_module/FlowFM_lateral_sources.bc', 1, &
+      success = adduniformtimerelation_objects('lateral_discharge', '', 'lateral', '9', 'discharge', BC_FILENAME, 1, &
                                                1, test_array)
 
       block
@@ -70,9 +111,29 @@ contains
       real(dp), dimension(3) :: test_array = [1.0_dp, 2.0_dp, 3.0_dp]
 
 
+      call create_file(BC_FILENAME, [ &
+                       "[General]", &
+                       "    fileVersion           = 1.01", &
+                       "    fileType              = boundConds", &
+                       "", &
+                       "[forcing]", &
+                       "    name                  = 10", &
+                       "    function              = timeseries", &
+                       "    timeInterpolation     = block-to", &
+                       "    quantity              = time", &
+                       "    unit                  = seconds since 2000-01-01 00:00:00", &
+                       "    quantity              = lateral_discharge", &
+                       "    unit                  = m³/s", &
+                       "    0 0", &
+                       "    10 100", &
+                       "    20 20", &
+                       "    30 300", &
+                       "    40 0"]);
+
       call initialize_ec_module()
 
-      success = adduniformtimerelation_objects('lateral_discharge', '', 'lateral', '10', 'discharge', 'ec_module/FlowFM_lateral_sources.bc', 1, &
+
+      success = adduniformtimerelation_objects('lateral_discharge', '', 'lateral', '10', 'discharge', BC_FILENAME, 1, &
                                                1, test_array)
 
       block
