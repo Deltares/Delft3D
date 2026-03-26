87c25108d95eee0b24bca38017ecdc01d07ad6b1
Mihai Pricope <mihai.pricope@deltares.nl>
2026-03-26 10:46:15 +0000

    UNST-9752: sleep before running tests
    Trying to prevent windows antivirus blocking the file

--- ci/teamcity/Delft3D/windows/test.kt
+++ ci/teamcity/Delft3D/windows/test.kt
@@ -100,6 +100,8 @@ object WindowsTest : BuildType({
                     Expand-Archive -Path %file_path% -DestinationPath "temp_extract"
 
                     robocopy "temp_extract/x64" ${'$'}dest /E /XC /XN /XO
+
+                    Start-Sleep -Seconds 3
                 """.trimIndent()
             }
         }
