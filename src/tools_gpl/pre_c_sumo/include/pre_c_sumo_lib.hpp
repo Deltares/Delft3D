5b764059a98fdc029c8b37e402168a21162af059
Adri Mourits <adri.mourits@deltares.nl>
2026-04-01 09:12:41 +0200

    UNST-9751: pre_c_sumo_lib: Added: timeloop

--- src/tools_gpl/pre_c_sumo/include/pre_c_sumo_lib.hpp
+++ src/tools_gpl/pre_c_sumo/include/pre_c_sumo_lib.hpp
@@ -1,7 +1,9 @@
 #ifndef SRC_TOOLS_GPL_PRE_C_SUMO_PRE_C_SUMO_LIB_HPP
 #define SRC_TOOLS_GPL_PRE_C_SUMO_PRE_C_SUMO_LIB_HPP
+#include <expected>
 
 #include <string_view>
+#include "csumo_settings_reader.hpp"
 
 namespace pre_c_sumo
 {
@@ -20,6 +22,16 @@ namespace pre_c_sumo
      * @return int Return 0 on success
      */
     int run();
+
+    bool do_timeloop();
+    std::expected<pre_c_sumo::CSumoSettingsReader, pre_c_sumo::ParseError> read_csumo_config_file(
+        const std::string_view);
+    void receive_ff_data();
+    void write_ff2nf_files();
+    void wait_for_nf2ff_files();
+    void read_nf2ff_files();
+    void convert_nf_to_sources_sinks();
+    void send_sources_sinks_to_ff();
 } // namespace pre_c_sumo
 
 #endif // SRC_TOOLS_GPL_PRE_C_SUMO_PRE_C_SUMO_LIB_HPP
