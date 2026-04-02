04175e93ab3b6b042ee07ff85d1f4ded241f5256
FlorisBuwaldaDeltares <floris.buwalda@Deltares.nl>
2026-04-02 12:09:06 +0200

    fix jsferic ambiguity in findtri_kdtree

--- src/utils_lgpl/ec_module/packages/ec_module/src/ec_basic_interpolation.F90
+++ src/utils_lgpl/ec_module/packages/ec_module/src/ec_basic_interpolation.F90
@@ -794,7 +794,7 @@ contains
 
    !>    find triangle for interpolation with kdtree
    !>       will initialize kdtree and triangulation connectivity
-   subroutine findtri_kdtree(XP, YP, ZP, XS, YS, ZS, NS, NDIM, NRFIND, INTRI, JSLO, SLO, JATEK, jadum, ierror, ind, wf, dmiss, jsferic_input, jins, jasfer3D)
+   subroutine findtri_kdtree(XP, YP, ZP, XS, YS, ZS, NS, NDIM, NRFIND, INTRI, JSLO, SLO, JATEK, jadum, ierror, ind, wf, dmiss, jsferic, jins, jasfer3D)
       implicit none
 
       real(kind=hp), intent(in) :: xp, yp !< node coordinates
@@ -832,9 +832,8 @@ contains
       real(kind=hp), external :: dcosphi
       real(kind=hp), intent(in) :: dmiss
       integer, intent(in) :: jins
-      integer, intent(in) :: jsferic_input
+      integer, intent(in) :: jsferic
       integer, intent(in) :: jasfer3D
-      integer :: jsferic
       real(kind=hp), parameter :: dfac = 1.000001d0 ! enlargement factor for pinpok3D
 
       ierror = 1
@@ -861,9 +860,6 @@ contains
          call aerr('LNtri(2,numedge)', ierr, 2 * numedge)
          LNtri = 0
 
-         !           dlaun is not spherical proof: set global jsferic for circumcenter
-         jsferic = 0
-
          do i = 1, numtri
             do ii = 1, 3
                !                 generate edge-triangle connectivity
@@ -884,19 +880,16 @@ contains
 
             !              compute triangle circumcenter
             if (jasfer3D == 1) then
-               call ave3D(3, xv, yv, xx(i), yy(i), jsferic_input, jasfer3D)
+               !jsferic = 0, dlaun output is not spherical safe!
+               call ave3D(3, xv, yv, xx(i), yy(i), 0, jasfer3D)
             else
                xx(i) = sum(xv(1:3)) / 3d0
                yy(i) = sum(yv(1:3)) / 3d0
             end if
          end do
 
-         !        restore jsferic
-         jsferic = jsferic_input
-
          call build_kdtree(treeglob, numtri, xx, yy, ierror, jsferic, dmiss)
 
-         !           deallocate
          deallocate (xx, yy)
 
          if (ierror /= 0) then
@@ -937,7 +930,7 @@ contains
 
 !        get a point in the cell
             if (jasfer3D == 1) then
-               call ave3D(3, xv, yv, xz, yz, jsferic, jasfer3D)
+               call ave3D(3, xv, yv, xz, yz, 0, jasfer3D)
             else
                xz = sum(xv(1:3)) / 3d0
                yz = sum(yv(1:3)) / 3d0
@@ -960,10 +953,6 @@ contains
                NRFIND = i
                exit mainloop
             end if
-
-            !           dlaun is not spherical proof: set global jsferic
-            jsferic = 0
-
             !           proceed to next triangle, which is adjacent to the edge that is cut by the line from the current triangle to the query point
             inext = 0
 
@@ -982,9 +971,9 @@ contains
                k1 = edgeindx(1, iedge)
                k2 = edgeindx(2, iedge)
                if (jasfer3D == 0) then
-                  call CROSS(xz, yz, xp, yp, xs(k1), ys(k1), xs(k2), ys(k2), JACROS, SL, SM, XCR, YCR, CRP, jsferic, dmiss)
+                  call CROSS(xz, yz, xp, yp, xs(k1), ys(k1), xs(k2), ys(k2), JACROS, SL, SM, XCR, YCR, CRP, 0, dmiss)
                else
-                  call cross3D(xz, yz, xp, yp, xs(k1), ys(k1), xs(k2), ys(k2), jacros, sL, sm, xcr, ycr, jsferic_input, dmiss)
+                  call cross3D(xz, yz, xp, yp, xs(k1), ys(k1), xs(k2), ys(k2), jacros, sL, sm, xcr, ycr, 0, dmiss)
                end if
 
                !              use tolerance
@@ -1018,6 +1007,7 @@ contains
          !            call qnerror('findtri_kdtree: error', ' ', ' ')
       end if
 
+      !> interploation doesn't use dlaun coordinates, so it uses original jsferic
       if (intri == 1) then
          if (jasfer3D == 0) then
             call interpolate_linear_from_triangle(xv, yv, zv, NDIM, xp, yp, zp, JSLO, SLO, wf, dmiss, jsferic)
