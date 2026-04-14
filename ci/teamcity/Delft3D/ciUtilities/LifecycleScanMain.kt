5d32646e96a60ec7b9c0ebb8539fe8e79f93a824
Bart Steur <bart.steur@deltares.nl>
2026-04-14 10:51:43 +0200

    main product

--- ci/teamcity/Delft3D/ciUtilities/LifecycleScanMain.kt
+++ ci/teamcity/Delft3D/ciUtilities/LifecycleScanMain.kt
@@ -26,7 +26,7 @@ object LifecycleScanMain : BuildType({
         step {
             id = "LifecycleNexusIqLinux"
             type = "LifecycleNexusIqLinux"
-            param("nexus_iq_application_id", "delft3d")
+            param("nexus_iq_application_id", "delft3d-main")
             param("nexus_iq_username", "%nexus_iq_username%")
             param("nexus_iq_password", "%nexus_iq_password%")
             param("exclude_dirs", "test/**,ci/**,doc/**,tools/**")
