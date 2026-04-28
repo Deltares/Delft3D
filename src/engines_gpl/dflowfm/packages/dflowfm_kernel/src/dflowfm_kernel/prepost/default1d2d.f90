d907fb971fbadc2236d59a128ce87448e03afa03
René van Klink <rene.vanklink@deltares.nl>
2026-04-28 15:56:39 +0000

    UNST-9384: Refactor m_default1d2d code

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/default1d2d.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/default1d2d.f90
@@ -1,92 +1,146 @@
 module m_default1d2d
    implicit none
 
+   private
+   public add_default_cross_sections_for_1d2d_links
+
 contains
 
-   subroutine set_default_1d2d_crs(network)
+   !> Some 1D2D links require 'default' cross section in the 1D channel administration.
+   !! This subroutine installs the required cross section definitions, adds an instance
+   !! of this cross section, and assigns the default cross sections to the 1D2D links.
+   subroutine add_default_cross_sections_for_1d2d_links(network)
       use precision, only: dp
       use m_network, only: t_network
-      use m_flowgeom, only: wu1Duni5, hh1Duni5
-      use m_physcoef, only: frcunistreetinlet
-      use m_CrossSections, only: AddCrossSectionDefinition, realloc
-      use network_data, only: numl1d, kn
+      use network_data, only: kn, numl1d, LINK_1D2D_STREETINLET
       use m_GlobalParameters, only: t_chainage2cross
       use m_readcrosssections, only: finalizeCrs
 
-      type(t_network), target, intent(inout) :: network
+      use m_flowgeom, only: wu1Duni5, hh1Duni5
+      use m_physcoef, only: frcunistreetinlet
+      use m_Roughness, only: R_MANNING
 
-      ! Local variables
+      type(t_network), target, intent(inout) :: network !< The 1D channel administration.
+
       integer :: idef, icrs, L
-      integer :: numLevels
-      real(kind=dp) :: level(2)
-      real(kind=dp) :: width(2)
+
+      call add_1d2d_cross_section_definition( &
+         network%CSDefinitions, width=wu1Duni5, height=hh1Duni5, &
+         friction_type=R_MANNING, friction_value=frcunistreetinlet, idef=idef &
+      )
+      call add_cross_section(network, idef, icrs)
+
+      call realloc_line2cross(network%adm%line2cross, [numl1d, 3])
+      do L = 1, numl1d
+         associate (link_cross_sections => network%adm%line2cross(L, :))
+            ! Street inlets share their link code `LINK_1D2D_STREETINLET` with long culverts. We assume that
+            ! at this point long culverts already have their cross sections installed in `line2cross`.
+            if (kn(3, L) == LINK_1D2D_STREETINLET .and. all(link_cross_sections%c1 == -1)) then
+               link_cross_sections = t_chainage2cross(c1=icrs, c2=icrs, f=1.0_dp, distance=0.0_dp)
+            end if
+         end associate
+      end do
+   end subroutine add_default_cross_sections_for_1d2d_links
+
+   !> Add a rectangular closed cross section, with a friction section, to the definition set.
+   subroutine add_1d2d_cross_section_definition(cs_defs, width, height, friction_type, friction_value, idef)
+      use precision, only: dp
+      use m_CrossSections, only: t_CSDefinitionSet, AddCrossSectionDefinition
+
+      type(t_CSDefinitionSet), intent(inout) :: cs_defs !< Cross section definition set to add the new cross section to.
+      real(kind=dp), intent(in) :: width !< Width of the new (rectangular) cross section definition.
+      real(kind=dp), intent(in) :: height !< Height of the new (rectagular) cross section definition.
+      integer, intent(in) :: friction_type !< Friction type of the new cross section definition. See `m_roughness` for examples.
+      real(kind=dp), intent(in) :: friction_value !< Friction value of the new cross section definition.
+      integer, intent(out) :: idef !< The returned identifier of the new cross section definition.
+
+      real(kind=dp) :: heights(2)
+      real(kind=dp) :: widths(2)
       real(kind=dp) :: plains(3)
-      real(kind=dp) :: crestLevel, baseLevel, flowArea, totalArea
-      logical :: closed
-      logical :: groundlayerUsed
-      real(kind=dp) :: groundlayer
-      character(len=:), allocatable :: id
-      integer, parameter :: MANNING = 1
-      type(t_chainage2cross), dimension(:, :), allocatable :: temp_line2cross
 
       ! Create a default 1D2D rectangular cross-section definition
-      id = 'default_1d2d_rect'
-      closed = .true.
-      groundlayerUsed = .false.
-      groundlayer = 0.0_dp
-
-      ! Set up rectangular profile with wu1Duni5 (width) and hh1Duni5 (height)
-      numLevels = 2 ! closed rectangular section has 2 levels
-
-      level(1) = 0.0_dp ! Bottom level
-      level(2) = hh1Duni5 ! Top level (height)
-      width(1) = wu1Duni5 ! Width at bottom
-      width(2) = wu1Duni5 ! Width at top (same for rectangle)
-
-      plains = 0.0_dp !wu1Duni5
-      crestLevel = 0.0_dp
-      baseLevel = 0.0_dp
-      flowArea = 0.0_dp
-      totalArea = 0.0_dp
+      heights = [0.0_dp, height]
+      widths = width
+      plains = 0.0_dp
 
       ! Add the cross-section definition to the network
-      idef = AddCrossSectionDefinition(network%CSDefinitions, id, numLevels, level, width, &
-                                       width, plains, crestLevel, baseLevel, flowArea, totalArea, &
-                                       closed, groundlayerUsed, groundlayer)
+      idef = AddCrossSectionDefinition( &
+         cs_defs, id='default_1d2d_rect', numLevels=2, level=heights, &
+         flowWidth=widths, totalWidth=widths, plains=plains, &
+         crestLevel=0.0_dp, baseLevel=0.0_dp, flowArea=0.0_dp, totalArea=0.0_dp, &
+         closed=.true., groundlayerUsed=.false., groundlayer=0.0_dp &
+      )
 
       ! Set up friction section using default 1D2D friction values
-      network%CSDefinitions%CS(idef)%frictionSectionsCount = 1
-      allocate (network%CSDefinitions%CS(idef)%frictionSectionID(1))
-      allocate (network%CSDefinitions%CS(idef)%frictionSectionIndex(1))
-      allocate (network%CSDefinitions%CS(idef)%frictionType(1))
-      allocate (network%CSDefinitions%CS(idef)%frictionValue(1))
+      associate(cs_def => cs_defs%CS(idef))
+         cs_def%frictionSectionsCount = 1
+         cs_def%frictionSectionID = ['']
+         cs_def%frictionSectionIndex = [0]
+         cs_def%frictionType = [friction_type]
+         cs_def%frictionValue = [friction_value]
+      end associate
+   end subroutine add_1d2d_cross_section_definition
 
-      network%CSDefinitions%CS(idef)%frictionSectionID(1) = ''
-      network%CSDefinitions%CS(idef)%frictionSectionIndex(1) = 0
+   !> Add an instance of the cross section with index `idef` to the cross sections in the 1D channel `network`.
+   subroutine add_cross_section(network, idef, icrs)
+      use m_network, only: t_network
+      use m_readcrosssections, only: finalizeCrs
+      use m_CrossSections, only: realloc
 
-      ! Use default friction values for 1D2D links of type 5 (street inlets)
-      network%CSDefinitions%CS(idef)%frictionType(1) = MANNING
-      network%CSDefinitions%CS(idef)%frictionValue(1) = frcunistreetinlet
+      type(t_network), intent(inout) :: network !< The 1D channel network object.
+      integer, intent(in) :: idef !< The cross section definition index.
+      integer, intent(out) :: icrs !< The index of the new cross section instance.
 
-      ! Now create a cross-section instance
-      if (network%crs%count + 1 > network%crs%size) then
-         call realloc(network%crs)
-      end if
-      icrs = network%crs%count + 1
-      call finalizeCrs(network, network%crs%cross(icrs), idef, icrs)
-      temp_line2cross = network%adm%line2cross
-      deallocate (network%adm%line2cross)
-      allocate (network%adm%line2cross(numl1d, 3))
-      network%adm%line2cross(1:size(temp_line2cross, 1), :) = temp_line2cross
-      do L = 1, numl1d
-         if (kn(3, L) == 5 .and. all(network%adm%line2cross(L, :)%c1 == -1)) then !> don't overwrite long culvert lin2cross
-            network%adm%line2cross(L, :)%c1 = icrs
-            network%adm%line2cross(L, :)%c2 = icrs
-            network%adm%line2cross(L, :)%f = 1.0_dp
-            network%adm%line2cross(L, :)%distance = 0.0_dp
+      associate(cross_sections => network%crs)
+         if (cross_sections%count + 1 > cross_sections%size) then
+            call realloc(cross_sections)
          end if
-      end do
+         icrs = cross_sections%count + 1
+         call finalizeCrs(network, cross_sections%cross(icrs), idef, icrs)
+      end associate
+   end subroutine add_cross_section
 
-   end subroutine set_default_1d2d_crs
+   !> Re-allocate and reshape the `line2cross` to have `new_size`. If reallocation is necessary
+   !! then copy the old elements to the new array.
+   subroutine realloc_line2cross(line2cross, new_size, stat)
+      use m_GlobalParameters, only: t_chainage2cross
+
+      type(t_chainage2cross), dimension(:,:), pointer, intent(inout) :: line2cross !< Link to cross sections administration.
+      integer, dimension(2), intent(in) :: new_size !< New shape of the of the `line2cross` array.
+      integer, optional, intent(out) :: stat !< Status code returned to make error handling possible.
+
+      type(t_chainage2cross), dimension(:,:), pointer :: temp
+      integer, dimension(2) :: copy_size
+      integer :: stat_
+
+      stat_ = 0  ! Default `stat` value: No error
+
+      if (associated(line2cross)) then
+         if (all(new_size == ubound(line2cross))) then
+            return ! Array is already the right size. Nothing to do
+         end if
+      end if
+
+      ! Reallocation required
+      allocate(temp(1:new_size(1), 1:new_size(2)), stat=stat_)
+      if (stat_ /= 0) then
+         if (present(stat)) then
+            stat = stat_
+         end if
+         return
+      end if
+
+      ! Copy as many elements in old `line2cross` as possible
+      if (associated(line2cross)) then
+         copy_size = min(ubound(line2cross), new_size)
+         temp(1:copy_size(1), 1:copy_size(2)) = line2cross(1:copy_size(1), 1:copy_size(2))
+         deallocate(line2cross, stat=stat_)
+      end if
+
+      line2cross => temp
+      if (present(stat)) then
+         stat = stat_
+      end if
+   end subroutine realloc_line2cross
 
 end module m_default1d2d
