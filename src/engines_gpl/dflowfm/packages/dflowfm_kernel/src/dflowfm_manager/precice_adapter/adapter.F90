23b97e78487bcf70b0f167c16ac801da1ef6c87d
Adri Mourits <adri.mourits@deltares.nl>
2026-04-30 15:18:28 +0200

    UNST-9859: adapter.F90: Added: Comments

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_manager/precice_adapter/adapter.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_manager/precice_adapter/adapter.F90
@@ -1,3 +1,10 @@
+!> @file adapter.F90
+!! @brief preCICE adapter implementation for D-Flow FM.
+!! 
+!! This module implements a concrete adapter that extends the
+!! `precice_adapter_interface_t` and provides routines to construct,
+!! initialize, update (advance) and finalize a preCICE coupling instance.
+!! It also contains utilities to publish model quantities to preCICE meshes.
 module precice_adapter
    use precice_adapter_interface, only: precice_adapter_interface_t
    use precision, only: dp
@@ -9,12 +16,16 @@ module precice_adapter
    real(kind=dp), save ::  summed_time_progress !> Cumulative time progress since the last preCICE advance, used to determine when to call precicef_advance.
    public :: precice_adapter_t
 
+   !> Maximum length for preCICE standard name strings stored in quantity_t.
    integer, parameter :: MAX_STANDARD_NAME_LENGTH = 50
+
+   !> A single quantity that can be exchanged with preCICE.
    type :: quantity_t
       character(kind=c_char, len=MAX_STANDARD_NAME_LENGTH) :: standard_name
-      logical :: is_active
+      logical :: is_active !> Flag indicating if this quantity should be published to preCICE
    end type quantity_t
    
+   !> Container with all quantities used by the adapter.
    type :: quantities_t
       type(quantity_t) :: bl = quantity_t(standard_name="sea_floor_depth_below_geoid", is_active=.true.)
       type(quantity_t) :: s1 = quantity_t(standard_name="sea_surface_height", is_active=.true.)
@@ -22,6 +33,8 @@ module precice_adapter
       type(quantity_t) :: rho = quantity_t(standard_name="sea_water_potential_density", is_active=.true.)
    end type quantities_t
    
+   !> Concrete preCICE adapter implementation.
+   !! Extends `precice_adapter_interface_t`.
    type, extends(precice_adapter_interface_t) :: precice_adapter_t
       character(kind=c_char, len=:), allocatable :: config_file
       character(kind=c_char, len=:), allocatable :: name
@@ -44,12 +57,16 @@ module precice_adapter
       procedure :: finalize => precice_adapter_finalize
    end type precice_adapter_t
 
+   !> Constructor interface for `precice_adapter_t`.
    interface precice_adapter_t
       procedure :: precice_adapter_constructor
    end interface
 
 contains
 
+   !> Constructor for `precice_adapter_t`.
+   !! Allocates and populates a new adapter instance with the provided settings and mesh coordinates.
+   !! @return pointer to newly allocated `precice_adapter_t` instance.
    function precice_adapter_constructor(config_file, name, is_communicator_set, communicator, my_rank, number_of_ranks, cell_center_mesh_name, cell_center_mesh_3d_name, &
                                         mesh_size, mesh_3d_size, cell_center_mesh_coordinates_2d, cell_center_mesh_coordinates_3d) result(adapter_instance)
       use precision, only: dp
@@ -88,6 +105,9 @@ contains
 
    end function precice_adapter_constructor
 
+   !> Initialize the preCICE adapter and preCICE participant.
+   !! Creates the preCICE participant (with or without communicator), registers mesh vertices,
+   !! produces initial data if required, and calls preCICE initialize.
    subroutine precice_adapter_initialize(self)
       use precice, only: precicef_set_vertices, precicef_initialize, &
                          precicef_create_with_communicator, precicef_create, &
@@ -120,6 +140,11 @@ contains
       summed_time_progress = 0.0
    end subroutine precice_adapter_initialize
 
+   !> Advance the coupling when the accumulated model_time reaches the preCICE time_window.
+   !! Accumulate timesteps, if the preCICE time_window is reached:
+   !! - Remesh the 3D mesh
+   !! - Write data (2D and 3D) to preCICE
+   !! - Advance preCICE
    subroutine precice_adapter_update(self, timestep)
       use precice, only: precicef_get_max_time_step_size, precicef_advance, &
                          precicef_is_coupling_ongoing, precicef_is_time_window_complete, &
@@ -134,7 +159,8 @@ contains
       class(precice_adapter_t), intent(inout) :: self
       real(kind=dp), intent(in) :: timestep
 
-      integer(kind=c_int) :: is_ongoing, is_time_window_complete
+      integer(kind=c_int) :: is_ongoing
+      integer(kind=c_int) :: is_time_window_complete
       real(kind=c_double) :: max_timestep
 
       call precicef_is_coupling_ongoing(is_ongoing)
@@ -142,13 +168,14 @@ contains
          return ! Skip if the connection is no longer ongoing.
       end if
 
-      ! Actually advance time
+      ! Update summed time progress and check if we need to advance preCICE
       call precicef_get_max_time_step_size(max_timestep)
       summed_time_progress = summed_time_progress + timestep
       if (summed_time_progress > max_timestep + 1.0) then
          call mess(LEVEL_ERROR, "Summed user time steps are beyond the preCICE coupling window!")
       end if
       if (summed_time_progress > max_timestep - 1.0e-5) then
+         ! Remesh the 3D mesh; that only works if the time window is complete (i.e. preCICE is ready for a new mesh).
          call precicef_is_time_window_complete(is_time_window_complete)
          if (is_time_window_complete == 1) then
             call precicef_reset_mesh(self%cell_center_mesh_3d_name, len(self%cell_center_mesh_3d_name))
@@ -157,12 +184,14 @@ contains
          end if
          call precice_adapter_write_data(self)
          call precicef_advance(max_timestep)
+         ! Reset summed_time_progress after advancing
          summed_time_progress = 0.0
       else
          write(*,*) "Not advancing preCICE yet, summed_time_progress = ", summed_time_progress, " max_timestep = ", max_timestep
       end if
    end subroutine precice_adapter_update
 
+   !> Finalize the preCICE adapter and perform preCICE shutdown.
    subroutine precice_adapter_finalize(self)
       use precice, only: precicef_finalize
       implicit none(type, external)
@@ -173,6 +202,12 @@ contains
       call precicef_finalize()
    end subroutine precice_adapter_finalize
 
+   !> Write the currently active model quantities to the registered preCICE meshes.
+   !! Quantities published:
+   !! - `hs` (sea_floor_depth_below_sea_surface) written to 2D mesh when active.
+   !! - `s1` (sea_surface_height) written to 2D mesh when active.
+   !! - `bl` (sea_floor_depth_below_geoid) written to 2D mesh when active (note the sign inversion).
+   !! - `rho` (sea_water_potential_density) written to 3D mesh when active.
    subroutine precice_adapter_write_data(self)
       use precice, only: precicef_write_data
       use precision, only: dp
@@ -183,7 +218,6 @@ contains
       implicit none(type, external)
       class(precice_adapter_t), intent(in) :: self
 
-      ! Write water depths (do we need to consider active nodes?)
       if (self%quantities%hs%is_active) then
          call precicef_write_data(self%cell_center_mesh_name, self%quantities%hs%standard_name, &
                                   size(self%vertex_ids), self%vertex_ids, &
@@ -206,4 +240,5 @@ contains
       end if
    end subroutine precice_adapter_write_data
 
-end module precice_adapter
+   end module precice_adapter
+   
