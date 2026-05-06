48f0347b787bbf30cf8ef264e31259fcfee41811
Willem Ottevanger <willem.ottevanger@deltares.nl>
2026-05-06 17:17:56 +0200

    Update resource.h
    Using Copilot:
    
    Interpreted the text as UTF‑16LE misread as UTF‑8/ANSI
    Mapped the mojibake sequences back to their standard Visual Studio RC header template
    Preserved exact semantics and constants
    
    This header is boilerplate generated by Visual Studio for .rc (resource) files and is safe to regenerate or replace.

--- src/engines_gpl/fbc/packages/FBCTools/src/resource.h
+++ src/engines_gpl/fbc/packages/FBCTools/src/resource.h
@@ -1,8 +1,12 @@
-//{{NO_DEPENDENCIES}}਍⼀⼀ 䴀椀挀爀漀猀漀昀琀 嘀椀猀甀愀氀 䌀⬀⬀ 最攀渀攀爀愀琀攀搀 椀渀挀氀甀搀攀 昀椀氀攀⸀ഀഀ
-// Used by Version.rc਍⼀⼀ഀഀ
-਍⼀⼀ 一攀砀琀 搀攀昀愀甀氀琀 瘀愀氀甀攀猀 昀漀爀 渀攀眀 漀戀樀攀挀琀猀ഀഀ
-// ਍⌀椀昀搀攀昀 䄀倀匀吀唀䐀䤀伀开䤀一嘀伀䬀䔀䐀ഀഀ
-#ifndef APSTUDIO_READONLY_SYMBOLS਍⌀搀攀昀椀渀攀 开䄀倀匀开一䔀堀吀开刀䔀匀伀唀刀䌀䔀开嘀䄀䰀唀䔀        ㄀　㄀ഀഀ
-#define _APS_NEXT_COMMAND_VALUE         40001਍⌀搀攀昀椀渀攀 开䄀倀匀开一䔀堀吀开䌀伀一吀刀伀䰀开嘀䄀䰀唀䔀         ㄀　　㄀ഀഀ
-#define _APS_NEXT_SYMED_VALUE           101਍⌀攀渀搀椀昀ഀഀ
-#endif਍
+
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+//
+// Used by Version.rc
+//
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
