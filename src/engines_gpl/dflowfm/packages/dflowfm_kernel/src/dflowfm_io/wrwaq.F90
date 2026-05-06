cad6bab4b91c3c48bf16d1a92cf5a2dccfe2b3fe
DIRECTORY\groenenb <Julien.Groenenboom@deltares.nl>
2026-05-06 09:59:21 +0200

    UNST-9914: correct the layer keywords in the .hyd file

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_io/wrwaq.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_io/wrwaq.F90
@@ -470,9 +470,9 @@ contains
       write (lunhyd, '(a,a)') 'task      ', 'full-coupling'
 
       if (layertype == LAYTP_SIGMA) then ! sigma-layers
-         write (lunhyd, '(a,a)') 'geometry  ', 'unstructured sigma-layers'
+         write (lunhyd, '(a,a)') 'geometry  ', 'unstructured'
       elseif (layertype == LAYTP_Z) then ! z- or z-sigma-layers
-         write (lunhyd, '(a,a)') 'geometry  ', 'unstructured z- or z-sigma-layers'
+         write (lunhyd, '(a,a)') 'geometry  ', 'unstructured z-layers'
       elseif (layertype == LAYTP_POLYGON_MIXED) then
          write (lunhyd, '(a,a)') 'geometry  ', 'unstructured polygon defined z-layers'
       elseif (layertype == LAYTP_DENS_SIGMA) then
