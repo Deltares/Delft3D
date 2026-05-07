3e3903ebcda3b676ea469e82653125d630b7352c
FlorisBuwaldaDeltares <117371974+FlorisBuwaldaDeltares@users.noreply.github.com>
2026-05-07 13:00:55 +0200

    Update src/engines_gpl/dflowfm/packages/dflowfm_lib/src/unstruc_bmi.F90
    Co-authored-by: Arthur van Dam <arthur.vandam@deltares.nl>

--- src/engines_gpl/dflowfm/packages/dflowfm_lib/src/unstruc_bmi.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_lib/src/unstruc_bmi.F90
@@ -2050,8 +2050,8 @@ contains
       ! Store the name and convert var and field to lowercase to make them case-insensitive.
       var_name = str_tolower(char_array_to_string(c_var_name))
       item_name = char_array_to_string(c_item_name)
-      field_name = str_tolower(char_array_to_string(c_field_name))
       field_name_original = char_array_to_string(c_field_name)
+      field_name = str_tolower(field_name_original)
 
       select case (var_name)
          ! PUMPS
