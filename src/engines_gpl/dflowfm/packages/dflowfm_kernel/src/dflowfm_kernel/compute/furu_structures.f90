64bb77deaf94397ef060a50c8d073df0de41a036
Jan Noort <jan.noort@deltares.nl>
2026-05-13 08:55:14 +0200

    UNST-9510 New references added
    UNST-9510: 2 full scale testcases updated
    
    UNST-9510 Blank space commit
    
    UNST-9510: update test cases

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/furu_structures.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/furu_structures.f90
@@ -7,7 +7,7 @@
 !  Delft3D is free software: you can redistribute it and/or modify
 !  it under the terms of the GNU Affero General Public License as
 !  published by the Free Software Foundation version 3.
-!
+!f
 !  Delft3D  is distributed in the hope that it will be useful,
 !  but WITHOUT ANY WARRANTY; without even the implied warranty of
 !  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
