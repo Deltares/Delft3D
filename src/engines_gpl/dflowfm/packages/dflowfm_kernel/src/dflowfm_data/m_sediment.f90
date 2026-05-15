b69d7b14ab4a4aea8bf75bcfa0128c869c8a2d5d
ZetaTam-dl <zeta.tam@deltares.nl>
2026-05-15 13:08:25 +0200

    restart pipeline

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/m_sediment.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/m_sediment.f90
@@ -220,7 +220,7 @@ contains
    
    !> A function to return the sediment density conversion factor depending on the keyword Transptype
    !! for converting the unit of the sediment transport quantities in the his/map writers.
-   !! For details, see the description of Transptype in Table A.5 in D-MOR Manual.
+   !! For details, see the description of Transptype in Table A.5 in D-MOR Manual. 
    pure function get_rhol(stmpar, idx) result(rhol)
       type(stmtype), intent(in)     :: stmpar           !< parameters in sediment-transport-morphology module
       integer, intent(in)           :: idx              !< index of sediment fractions
