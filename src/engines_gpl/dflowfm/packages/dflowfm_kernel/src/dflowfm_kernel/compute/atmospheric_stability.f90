af9100bb2002d8e2dfd1f8bba13eab1576121caf
Julius Sumihar <julius.sumihar@deltares.nl>
2026-05-24 20:25:27 +0200

    UNST-9885: apply 1st review feedback

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/atmospheric_stability.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/atmospheric_stability.f90
@@ -1,6 +1,26 @@
 module m_atmospheric_stability
+!! This module computes bulk air-sea exchange fluxes (wind stress, latent and sensible heat) using Monin-Obukhov 
+!! Similarity Theory.
+!! 
+!! The algorithm iteratively solves for the turbulence scaling parameters (u*, t*, q*)
+!! by updating roughness lengths and stability corrections (psi functions) until convergence. 
+!! From these, surface fluxes are derived via:
+!!
+!!   tau   = rho_air * u*^2
+!!   Qh    = rho_air * Cp * u* * t*
+!!   Qe    = rho_air * Lv * u* * q*
+!!
+!! Inputs required:
+!!   - Wind velocity components (u, v)
+!!   - Air temperature
+!!   - Dew-point temperature (used to derive vapor pressure and specific humidity)
+!!   - Air pressure at surface
+!!   - Sea surface temperature
+!!   - Charnock parameter 
    use precision, only: dp
    use m_sferic, only: pi
+   use m_globalparameters, only: gravity
+   use m_physcoef, only: vonkarw
    
    implicit none(type, external)
    
@@ -25,8 +45,6 @@ module m_atmospheric_stability
    real(kind=dp), parameter :: HEIGHT_WIND_VELOCITY = 10.0_dp ! sensor height for wind velocity [m]
    real(kind=dp), parameter :: HEIGHT_TEMPERATURE = 2.0_dp ! sensor height for temperature [m]
    real(kind=dp), parameter :: HEIGHT_HUMIDITY = 2.0_dp ! sensor height for specific humidity [m]
-   real(kind=dp), parameter :: CONST_GRAVITY = 9.80665_dp ! gravitational acceleration [m/s2]
-   real(kind=dp), parameter :: CONST_KAPPA = 0.4_dp ! von Kármán constant [-]
    real(kind=dp), parameter :: CONST_R = 8.31451_dp ! universal gas constant [J/(mol K)]
    real(kind=dp), parameter :: CONST_Md = 28.9644e-3_dp ! dry air molar mass [kg/mol]
    real(kind=dp), parameter :: CONST_Mv = 18.0153e-3_dp ! water vapor molar mass [kg/mol]
@@ -39,12 +57,14 @@ module m_atmospheric_stability
    real(kind=dp), parameter :: CONST_E0 = 611.21_dp ! water vapour saturation pressure over water (Pa) at triple point temperature (Tt) [Pa]
    real(kind=dp), parameter :: CONST_TT = 273.16_dp ! triple point temperature [K]
    real(kind=dp), parameter :: OBUKHOV_LENGTH_LIMIT = 1.0e4_dp ! limit of Obukhov length to prevent numerical issues [m]
+   real(kind=dp), parameter :: SMALL_NUMBER = 1.0e-8_dp
    
    !> Optional switches controlling physical parameterizations.
+   !! Enable terms only when they are needed for the target conditions.
    type :: t_options
-      logical :: include_free_convection = .false.  !< Include free convection.
-      logical :: include_fqsat = .false.            !< Include reduction factor of saturation specific humidity due to salt.
-      logical :: include_monin_obukhov = .true.     !< Include atmospheric stability in computing scaling parameters.
+      logical :: include_free_convection = .false.  !< Use in weak-wind unstable conditions (buoyancy-driven turbulence matters); usually unnecessary in moderate/strong wind or neutral/stable cases.
+      logical :: include_fqsat = .false.            !< Use when representing salinity effects on air-sea fluxes by reducing surface saturation specific humidity.
+      logical :: include_monin_obukhov = .true.     !< Use for stability-aware similarity corrections; disable for neutral conditions.
    end type t_options
 
    !> Scaling parameters data type.
@@ -52,7 +72,7 @@ module m_atmospheric_stability
       real(kind=dp) :: u_star = 0.0_dp                 !< Friction velocity [m/s].
       real(kind=dp) :: t_star = 0.0_dp                 !< Temperature scale [K].
       real(kind=dp) :: q_star = 0.0_dp                 !< Humidity scale [kg/kg].
-      real(kind=dp) :: w_star = 0.0_dp                 !< Gustiness velocity [m/s].
+      real(kind=dp) :: w_star = 0.0_dp                 !< Free convective velocity scale [m/s].
       real(kind=dp) :: monin_obukhov_length = 0.0_dp   !< Obukhov length [m].
       real(kind=dp) :: richardson_number = 0.0_dp      !< Bulk Richardson number [-].
       real(kind=dp) :: z0_momentum = 0.0_dp            !< Momentum roughness [m].
@@ -74,8 +94,8 @@ module m_atmospheric_stability
 contains
 
    !> Compute turbulence scaling parameters for a point value.
-   subroutine compute_scaling_parameters(wind_velocity_u, wind_velocity_v, air_temperature, dew_point_temperature, &
-                                  air_pressure, charnock, surface_temperature, result, options)
+   pure function compute_scaling_parameters(wind_velocity_u, wind_velocity_v, air_temperature, dew_point_temperature, &
+                                  air_pressure, charnock, surface_temperature, options) result(result)
       real(kind=dp), intent(in) :: wind_velocity_u         !< U wind component [m/s].
       real(kind=dp), intent(in) :: wind_velocity_v         !< V wind component [m/s].
       real(kind=dp), intent(in) :: air_temperature         !< Air temperature [K].
@@ -83,10 +103,9 @@ contains
       real(kind=dp), intent(in) :: air_pressure            !< Air pressure [Pa].
       real(kind=dp), intent(in) :: charnock                !< Charnock parameter [-].
       real(kind=dp), intent(in) :: surface_temperature     !< Surface temperature [K].
-      type(t_scales), intent(out) :: result                !< Computed scaling outputs.
-      type(t_options), intent(in), optional :: options     !< Optional model switches.
+      type(t_options), intent(in) :: options     !< Optional model switches.
+      type(t_scales) :: result                !< Computed scaling outputs.
 
-      type(t_options) :: runtime_options
       real(kind=dp) :: salt_saturation_humidity_reduction_factor
       real(kind=dp) :: wind_velocity_magnitude
       real(kind=dp) :: vapor_pressure, saturated_vapor_pressure
@@ -102,7 +121,6 @@ contains
       real(kind=dp) :: obukhov_length, richardson_number
       real(kind=dp) :: z0_momentum, z0_heat, z0_humidity
       real(kind=dp) :: u_star, t_star, q_star
-      real(kind=dp) :: u_star_new, t_star_new, q_star_new
       real(kind=dp) :: convergence_error
       real(kind=dp) :: log_denominator_momentum, log_denominator_heat, log_denominator_humidity
       real(kind=dp) :: heat_profile_correction, humidity_profile_correction
@@ -113,17 +131,11 @@ contains
       integer, parameter :: MAXIMUM_ITERATION = 50
       real(kind=dp), parameter :: EPSILON = 1.0e-4_dp
 
-      if (present(options)) then
-         runtime_options = options
-      else
-         runtime_options = t_options()
-      end if
-
       wind_velocity_magnitude = sqrt(wind_velocity_u**2 + wind_velocity_v**2)
       vapor_pressure = compute_saturation_pressure(dew_point_temperature)
       air_humidity = compute_specific_humidity(vapor_pressure, air_pressure)
       salt_saturation_humidity_reduction_factor = 1.0_dp
-      if (runtime_options%include_fqsat) then
+      if (options%include_fqsat) then
          salt_saturation_humidity_reduction_factor = 0.98_dp
       end if
       saturated_vapor_pressure = compute_saturation_pressure(surface_temperature)
@@ -131,12 +143,12 @@ contains
       surface_humidity = salt_saturation_humidity_reduction_factor * surface_humidity
       
       ! Convert temperature and specific humidity to equivalent values at wind sensor height using logarithmic profiles.
-      temperature_pfactor = 0.04_dp * log(HEIGHT_WIND_VELOCITY / HEIGHT_TEMPERATURE) / CONST_KAPPA
+      temperature_pfactor = 0.04_dp * log(HEIGHT_WIND_VELOCITY / HEIGHT_TEMPERATURE) / vonkarw
       temperature_at_wind_height = (air_temperature - temperature_pfactor*surface_temperature) / (1.0_dp - temperature_pfactor)
-      humidity_pfactor = 0.04_dp * log(HEIGHT_WIND_VELOCITY / HEIGHT_HUMIDITY) / CONST_KAPPA
+      humidity_pfactor = 0.04_dp * log(HEIGHT_WIND_VELOCITY / HEIGHT_HUMIDITY) / vonkarw
       humidity_at_wind_height = (air_humidity - humidity_pfactor*surface_humidity) / (1.0_dp - humidity_pfactor)
 
-      if (runtime_options%include_free_convection) then
+      if (options%include_free_convection) then
          convective_velocity_scale = 0.5_dp
          delta_wind_speed = sqrt(wind_velocity_magnitude**2 + convective_velocity_scale**2)
       else
@@ -160,18 +172,19 @@ contains
       do iteration = 1, MAXIMUM_ITERATION
          call compute_roughness_lengths(u_star, charnock, z0_momentum, z0_heat, z0_humidity)
 
-         if (runtime_options%include_free_convection) then
+         if (options%include_free_convection) then
             convective_velocity_scale = compute_convective_velocity_scale(u_star, t_star, q_star, &
                                                         temperature_at_wind_height, humidity_at_wind_height)
             delta_wind_speed = sqrt(wind_velocity_magnitude**2 + convective_velocity_scale**2)
          end if
 
-         if (runtime_options%include_monin_obukhov) then
+         if (options%include_monin_obukhov) then
              richardson_number = compute_richardson_number(delta_wind_speed, surface_temperature, &
                               temperature_at_wind_height, air_pressure, humidity_at_wind_height, &
                               salt_saturation_humidity_reduction_factor)
 
              log_denominator_momentum = log(HEIGHT_WIND_VELOCITY) - log(z0_momentum) - psi_momentum
+             log_denominator_momentum = sign(max(abs(log_denominator_momentum), SMALL_NUMBER), log_denominator_momentum)
              log_denominator_heat = log(HEIGHT_WIND_VELOCITY) - log(z0_heat) - psi_heat
 
              obukhov_length = (HEIGHT_WIND_VELOCITY / richardson_number) * log_denominator_heat / log_denominator_momentum**2
@@ -189,7 +202,7 @@ contains
                 heat_profile_correction = log(HEIGHT_WIND_VELOCITY) - log(HEIGHT_TEMPERATURE) &
                      - stability_profile_heat_humidity((HEIGHT_WIND_VELOCITY + z0_momentum) * inverse_obukhov_length) &
                      + stability_profile_heat_humidity((HEIGHT_TEMPERATURE + z0_momentum) * inverse_obukhov_length)
-                temperature_at_wind_height = air_temperature + (t_star / CONST_KAPPA) * heat_profile_correction
+                temperature_at_wind_height = air_temperature + (t_star / vonkarw) * heat_profile_correction
                 delta_temperature = temperature_at_wind_height - surface_temperature
              end if
 
@@ -197,7 +210,7 @@ contains
                 humidity_profile_correction = log(HEIGHT_WIND_VELOCITY) - log(HEIGHT_HUMIDITY) &
                      - stability_profile_heat_humidity((HEIGHT_WIND_VELOCITY + z0_momentum) * inverse_obukhov_length) &
                      + stability_profile_heat_humidity((HEIGHT_HUMIDITY + z0_momentum) * inverse_obukhov_length)
-                humidity_at_wind_height = air_humidity + (q_star / CONST_KAPPA) * humidity_profile_correction
+                humidity_at_wind_height = air_humidity + (q_star / vonkarw) * humidity_profile_correction
                 delta_specific_humidity = humidity_at_wind_height - surface_humidity
              end if
              
@@ -209,16 +222,22 @@ contains
             log_denominator_heat = log(HEIGHT_TEMPERATURE) - log(z0_heat)
             log_denominator_humidity = log(HEIGHT_HUMIDITY) - log(z0_humidity)
          end if
+         
+         log_denominator_momentum = sign(max(abs(log_denominator_momentum), SMALL_NUMBER), log_denominator_momentum)
+         log_denominator_heat = sign(max(abs(log_denominator_heat), SMALL_NUMBER), log_denominator_heat)
+         log_denominator_humidity = sign(max(abs(log_denominator_humidity), SMALL_NUMBER), log_denominator_humidity)
 
-         u_star_new = CONST_KAPPA * delta_wind_speed / log_denominator_momentum
-         t_star_new = CONST_KAPPA * delta_temperature / log_denominator_heat
-         q_star_new = CONST_KAPPA * delta_specific_humidity / log_denominator_humidity
+         result%u_star = vonkarw * delta_wind_speed / log_denominator_momentum
+         result%t_star = vonkarw * delta_temperature / log_denominator_heat
+         result%q_star = vonkarw * delta_specific_humidity / log_denominator_humidity
 
-         convergence_error = sqrt((u_star_new - u_star)**2 + (t_star_new - t_star)**2 + (q_star_new - q_star)**2)
+         convergence_error = sqrt(((result%u_star - u_star)/max(abs(u_star), SMALL_NUMBER))**2 + &
+                             ((result%t_star - t_star)/max(abs(t_star), SMALL_NUMBER))**2 + &
+                             ((result%q_star - q_star)/max(abs(q_star), SMALL_NUMBER))**2)
 
-         u_star = u_star_new
-         t_star = t_star_new
-         q_star = q_star_new
+         u_star = result%u_star
+         t_star = result%t_star
+         q_star = result%q_star
 
          if (iteration >= MINIMUM_ITERATION .and. convergence_error <= EPSILON) then
             exit
@@ -234,7 +253,7 @@ contains
       result%z0_momentum = z0_momentum
       result%z0_heat = z0_heat
       result%z0_humidity = z0_humidity
-   end subroutine compute_scaling_parameters
+   end function compute_scaling_parameters
 
    !> Compute arrays of scaling parameters and bulk surface fluxes.
    subroutine compute_scales_and_fluxes(wind_velocity_u, wind_velocity_v, air_temperature, dew_point_temperature, &
@@ -271,9 +290,9 @@ contains
       allocate(fluxes(number_of_elements))
 
       do index = 1, number_of_elements
-         call compute_scaling_parameters(wind_velocity_u(index), wind_velocity_v(index), air_temperature(index), &
+         scaling_parameter = compute_scaling_parameters(wind_velocity_u(index), wind_velocity_v(index), air_temperature(index), &
                                          dew_point_temperature(index), air_pressure(index), charnock(index), &
-                                         surface_temperature(index), scaling_parameter, options)
+                                         surface_temperature(index), options)
 
          vapor_pressure = compute_saturation_pressure(dew_point_temperature(index))
          air_density = compute_air_density(air_temperature(index), air_pressure(index), vapor_pressure)
@@ -503,10 +522,12 @@ contains
       real(kind=dp), parameter :: ALPHA_M = 0.11_dp ! roughness length coefficient for momentum
       real(kind=dp), parameter :: ALPHA_H = 0.40_dp ! roughness length coefficient for heat
       real(kind=dp), parameter :: ALPHA_Q = 0.62_dp ! roughness length coefficient for humidity
+      real(kind=dp) :: inverse_u_star
 
-      z0_momentum = ALPHA_M*CONST_NU_AIR/u_star + charnock*u_star*u_star/CONST_GRAVITY
-      z0_heat = ALPHA_H*CONST_NU_AIR/u_star
-      z0_humidity = ALPHA_Q*CONST_NU_AIR/u_star
+      inverse_u_star = 1.0_dp / sign(max(abs(u_star), SMALL_NUMBER), u_star)
+      z0_momentum = ALPHA_M*CONST_NU_AIR*inverse_u_star + charnock*u_star*u_star/gravity
+      z0_heat = ALPHA_H*CONST_NU_AIR*inverse_u_star
+      z0_humidity = ALPHA_Q*CONST_NU_AIR*inverse_u_star
    end subroutine compute_roughness_lengths
 
    !> Compute saturation-vapor-pressure from temperature (ECMWF_T2esat).
@@ -565,7 +586,7 @@ contains
       air_virtual_temperature = air_temperature * (1.0_dp + CONST_EST*air_specific_humidity)
       virtual_temperature_difference = air_virtual_temperature - surface_virtual_temperature
 
-      richardson_number = CONST_GRAVITY * virtual_temperature_difference * HEIGHT_WIND_VELOCITY / &
+      richardson_number = gravity * virtual_temperature_difference * HEIGHT_WIND_VELOCITY / &
                                max(air_virtual_temperature*wind_speed*wind_speed, 1.0e-10_dp)
    end function compute_richardson_number
 
@@ -578,19 +599,19 @@ contains
       real(kind=dp), parameter :: coef_c = 5.0_dp
       real(kind=dp), parameter :: coef_d = 0.35_dp
       real(kind=dp), parameter :: coef_unstable = 16.0_dp
-      real(kind=dp) :: clipped_zeta, unstable_factor, psi_unstable, psi_stable
+      real(kind=dp) :: clipped_zeta, unstable_factor
 
       clipped_zeta = min(stability_parameter, 5.0_dp)
-      unstable_factor = (1.0_dp - coef_unstable*clipped_zeta)**0.25_dp
-      psi_unstable = 2.0_dp * log((1.0_dp + unstable_factor*unstable_factor) / 2.0_dp)
-      psi_stable = (-coef_b*(clipped_zeta - (coef_c/coef_d))*exp(-coef_d*clipped_zeta)) - &
-                   (1.0_dp + (2.0_dp/3.0_dp)*coef_a*clipped_zeta)**1.5_dp - (coef_b*coef_c/coef_d) + 1.0_dp
-
       if (clipped_zeta < 0.0_dp) then
-         stability_correction = psi_unstable
+      ! unstable conditions
+         unstable_factor = (1.0_dp - coef_unstable*clipped_zeta)**0.25_dp
+         stability_correction = 2.0_dp * log((1.0_dp + unstable_factor*unstable_factor) / 2.0_dp)
       else if (clipped_zeta > 0.0_dp) then
-         stability_correction = psi_stable
+      ! stable conditions
+         stability_correction = (-coef_b*(clipped_zeta - (coef_c/coef_d))*exp(-coef_d*clipped_zeta)) - &
+                                (1.0_dp + (2.0_dp/3.0_dp)*coef_a*clipped_zeta)**1.5_dp - (coef_b*coef_c/coef_d) + 1.0_dp
       else
+      ! neutral conditions
          stability_correction = 0.0_dp
       end if
    end function stability_profile_heat_humidity
@@ -636,7 +657,7 @@ contains
 
       virtual_temperature_scale = temperature_scale*(1.0_dp + air_specific_humidity*CONST_EST) + &
                                   (CONST_EST*air_temperature*humidity_scale)
-      buoyancy_flux = max(0.0_dp, -CONST_GRAVITY*friction_velocity*virtual_temperature_scale / max(air_temperature, 1.0e-8_dp))
+      buoyancy_flux = max(0.0_dp, -gravity*friction_velocity*virtual_temperature_scale / max(air_temperature, 1.0e-8_dp))
       convective_velocity_scale = (buoyancy_flux*boundary_layer_height)**(1.0_dp/3.0_dp)
    end function compute_convective_velocity_scale
 
