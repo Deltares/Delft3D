3a9e2df12bb5ab0a9d460cb58d57dc23eae2a6c1
Willem Ottevanger <willem.ottevanger@deltares.nl>
2026-05-29 19:30:11 +0200

    UNST-9871 adjustments after review

--- src/utils_gpl/morphology/packages/morphology_kernel/test/test_morphology.f90
+++ src/utils_gpl/morphology/packages/morphology_kernel/test/test_morphology.f90
@@ -42,20 +42,20 @@ contains
       real(fp) :: d15 = 0.0001_fp ! D15 of the sediment [m]
       real(fp) :: di50 = 0.0002_fp ! D50 of the sediment [m]
       real(fp) :: d90 = 0.0003_fp ! D90 of the sediment [m]
-      real(fp) :: dzbdt = 0.1_fp !  erosion/sedimentation velocity [m/s]
+      real(fp) :: dzbdt = 0.001_fp ! erosion/sedimentation velocity [m/s]
       real(fp) :: dzdx = 0.01_fp ! slope in x direction [-]
       real(fp) :: dzdy = 0.0_fp ! slope in y direction [-]
       real(fp) :: h = 1.0_fp ! water depth [m]
       real(fp) :: hrms = 1.1_fp ! Root mean square wave height  [m]
       real(fp) :: kwtur = 0.1_fp ! Breaker induced turbulence [m^2/s^2]
       real(fp) :: poros = 0.4_fp ! Porosity of the sediment [-]
-      real(fp) :: rlabda = 0.5_fp ! Parameter from Ruessink et al. 2009 JGR
+      real(fp) :: rlabda = 60.0_fp ! Wave length [m]
       real(fp) :: teta = 0.1_fp ! angle between wave direction and x-axis [degrees]
       real(fp) :: tp = 5_fp ! Wave period   [s]
       real(fp) :: ubot = 0.1_fp ! velocity at the bed [m/s]
-      real(fp) :: u = 1.0_fp ! velocity in x direction [m/s]
+      real(fp) :: u = 0.1_fp ! velocity in x direction [m/s]
       real(fp) :: v = 0.0_fp ! velocity in y direction [m/s]
-      real(fp) :: vicmol = 1e-6 ! kinematic viscosity of water [m^2/s]
+      real(fp) :: vicmol = 1e-6_fp ! kinematic viscosity of water [m^2/s]
       ! output variables
       real(fp) :: sbcu
       real(fp) :: sbcv
@@ -74,15 +74,13 @@ contains
 
       real(fp), dimension(npar) :: par
 
-      real(fp) :: ag = 9.81_fp ! gravity acceleration [m/s^2]
-      real(fp) :: delta = 1.65_fp ! relative density of sediment [-]
+      real(fp), parameter :: ag = 9.81_fp ! gravity acceleration [m/s^2]
+      real(fp), parameter :: delta = 1.65_fp ! relative density of sediment [-]
 
       allocate (pardef(npardef))
       allocate (parkeyw(npardef))
 
       call traparams(iform, name, nparreq, nparopt, parkeyw, pardef)
-      ag = 9.81_fp ! gravity acceleration
-      delta = 1.65_fp ! relative density of sediment
 
       par(1) = ag
       par(4) = delta
@@ -95,12 +93,11 @@ contains
                 & poros, chezy, dzdx, dzdy, sbcu, sbcv, cesus, &
                 & ua, va, ubot, kwtur, ubot_from_com)
 
-      call f90_assert_near(sbcu, 6.8192570641767482e-05_fp, 1.0e-8_fp, "sbcu is not near the expected value"//c_null_char)
-      call f90_assert_near(sbcv, 3.1473055602818149e-08_fp, 1.0e-11_fp, "sbcv is not near the expected value"//c_null_char)
-      call f90_assert_near(cesus, 0.0012548999533282801_fp, 1.0e-7_fp, "cesus is not near the expected value"//c_null_char)
-      call f90_assert_near(ua, 0.35950490143587227_fp, 1.0e-4_fp, "ua is not near the expected value"//c_null_char)
-      call f90_assert_near(va, 0.00062745505782396465_fp, 1.0e-8_fp, "va is not near the expected value"//c_null_char)
-
+      call f90_assert_near(sbcu, 1.3433e-05_fp, 1.0e-08_fp, "sbcu is not near the expected value"//c_null_char)
+      call f90_assert_near(sbcv, 1.8343e-08_fp, 1.0e-11_fp, "sbcv is not near the expected value"//c_null_char)
+      call f90_assert_near(cesus, 5.2902e-04_fp, 1.0e-07_fp, "cesus is not near the expected value"//c_null_char)
+      call f90_assert_near(ua, 0.359505_fp, 1.0e-04_fp, "ua is not near the expected value"//c_null_char)
+      call f90_assert_near(va, 6.2746e-04_fp, 1.0e-08_fp, "va is not near the expected value"//c_null_char)
    end subroutine test_trab19
    !$f90tw)
 
