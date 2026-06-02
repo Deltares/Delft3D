cbb8cf565360f09d5f09a6a61d0d7a12a70bd1f7
FlorisBuwaldaDeltares <floris.buwalda@Deltares.nl>
2026-06-02 11:33:55 +0200

    instead of including a 0 in the average for missing values, we instead skip the whole averaging and write dmiss if any values were missing during the output interval.

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/m_statistical_output.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/m_statistical_output.f90
@@ -141,16 +141,24 @@ contains
    !> Update the stat_output of an item, depending on the operation_type.
    elemental subroutine update_statistical_output(item, dts)
       use precision, only: dp
+      use m_missing, only: dmiss
+      use precision_basics, only: equal
+
       type(t_output_variable_item), intent(inout) :: item !< statistical output item to update
       real(kind=dp), intent(in) :: dts !< current timestep
+
+      if (item%operation_type == SO_CURRENT) then
+         return
+      end if
+
+      item%missing = item%missing .or. equal(item%source_input, dmiss)
 
       if (item%operation_type == SO_MIN .or. item%operation_type == SO_MAX) then ! max/min of moving average requested
          call update_moving_average_data(item%moving_average_data, item%source_input, dts)
       end if
 
       select case (item%operation_type)
-      case (SO_CURRENT)
-         return
+
       case (SO_AVERAGE)
          item%stat_output = item%stat_output + item%source_input * dts
          item%time_step_sum = item%time_step_sum + dts
@@ -163,14 +171,19 @@ contains
       end select
    end subroutine update_statistical_output
 
-   !> Perform the final time interval averaging on an item,
-   !! after all values haven been summed up in %stat_output.
+   !> Perform the final time interval averaging on an item, after all values haven been summed up in %stat_output.
+   ! additionally, set the final output value to dmiss if any data was missing during the interval.
    elemental subroutine finalize_average(item)
+      use m_missing, only: dmiss
 
       type(t_output_variable_item), intent(inout) :: item !< The item to be processed. Will be double-checked on its operation type.
 
       if (item%operation_type == SO_AVERAGE) then
          item%stat_output = item%stat_output / item%time_step_sum
+      end if
+
+      if (any(item%operation_type == [SO_AVERAGE, SO_MIN, SO_MAX])) then ! missing array is allocated for any averaging output
+         item%stat_output(item%missing) = dmiss
       end if
 
    end subroutine finalize_average
@@ -194,6 +207,9 @@ contains
       case default
          return
       end select
+
+      item%missing = .false. ! reset missing flag for new output interval
+
    end subroutine reset_statistical_output
 
    !> Create a new output item and add it to the output set according to output quantity config
@@ -324,10 +340,9 @@ contains
       select case (item%operation_type)
       case (SO_CURRENT)
          item%stat_output => item%source_input
-      case (SO_AVERAGE)
+      case (SO_MIN, SO_MAX, SO_AVERAGE)
          allocate (item%stat_output(input_size))
-      case (SO_MIN, SO_MAX)
-         allocate (item%stat_output(input_size))
+         allocate(item%missing(input_size))
       case (SO_NONE)
          continue
       case default
