9336e2cabc3f7e06d1848ffaea123eb25ced9469
FlorisBuwaldaDeltares <floris.buwalda@Deltares.nl>
2026-06-11 11:31:34 +0200

    only write md_convertlongculverts when it is not default

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/unstruc_model.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/unstruc_model.f90
@@ -2722,7 +2722,7 @@ contains
       call prop_set(prop_ptr, 'General', 'fileVersion', trim(tmpstr), 'File format version (do not edit this)')
       call prop_set(prop_ptr, 'General', 'ModelSpecific', md_specific, 'Optional ''model specific ID'', to enable certain custom runtime function calls (instead of via MDU name).')
       call prop_set(prop_ptr, 'General', 'PathsRelativeToParent', md_paths_relto_parent, 'Default: 0. Whether or not (1/0) to resolve file names (e.g. inside the *.ext file) relative to their direct parent, instead of to the toplevel MDU working dir.')
-      if (writeall .or. md_convertlongculverts == 0) then      
+      if (md_convertlongculverts /= 1) then !> hidden keyword, only write when not default.      
          call prop_set(prop_ptr, 'General', 'ConvertLongCulverts', md_convertlongculverts, 'Whether or not (1/0) to convert long culvert input to 1D2D long culverts')
       end if
       ! Geometry
