4ab9f0cb18d4449af7b1f28250c22c309d873073
Sam Vente <sam.vente@deltares.nl>
2026-06-18 16:11:48 +0200

    fix: dimr config

--- ci/teamcity/Delft3D/linux/container_smoketest/ReceiveH7ContainerSmokeTest.kt
+++ ci/teamcity/Delft3D/linux/container_smoketest/ReceiveH7ContainerSmokeTest.kt
@@ -78,7 +78,7 @@ object LinuxReceiveH7ContainerSmokeTest : BuildType({
                     --compare 
                     --skip-run
                     --skip-download cases 
-                    --config configs/container_smoke_test/apptainer_lnx64.xml
+                    --config configs/container_smoke_tests/apptainer_lnx64.xml
                     --log-level INFO 
                     --teamcity 
                     --parallel
