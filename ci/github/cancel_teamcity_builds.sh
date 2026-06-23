9487900d3bc820bac2e218cacf8b996ec7ea6783
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2026-06-23 11:35:29 +0200

    DEVOPSDSC-985: Abort TC build cancellation GH workflow when the Trigger build config state is unknown (#986)

--- ci/github/cancel_teamcity_builds.sh
+++ ci/github/cancel_teamcity_builds.sh
@@ -222,12 +222,10 @@ function query_trigger() {
     kill_em_all=1
     ;;
   *)
-    printf "     %b Unknown state '%s'. Cannot proceed.\n" "${UNICODE_UNKNOWN}" "${build_state}" >&2
-    kill_em_all=0
+    printf "     %b Unknown state '%s'. The build state is likely not available yet. Cannot proceed. Trying again later may result in successful cancellation.\n" "${UNICODE_UNKNOWN}" "${build_state}" >&2
+    exit 1
     ;;
   esac
-
-  printf "%s\t%s\n" "${kill_em_all}" "${build_id}"
 }
 
 function cancel_all_builds() {
