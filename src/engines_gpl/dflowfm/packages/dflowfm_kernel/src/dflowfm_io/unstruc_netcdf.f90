481054ce958523683413a6c6e56a3d3106273c7a
Mihai Pricope <mihai.pricope@deltares.nl>
2026-06-30 06:35:20 +0000

    UNST-10022: reuse pump capacity from rst

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_io/unstruc_netcdf.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_io/unstruc_netcdf.f90
@@ -18215,6 +18215,7 @@ contains
                   istru = network%sts%pumpIndices(i)
                   pstru => network%sts%struct(istru)
                   pstru%pump%current_capacity = tmpvar(i)
+                  pstru%pump%capacity = tmpvar(i)
                end do
             end if
 
