62b021bd6dd653439e11017d814a21d4dc710061
René van Klink <rene.vanklink@deltares.nl>
2026-06-30 13:21:44 +0000

    UNST-9931: Add some more unit tests for get_constituent_name

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/test/src/test_fm_external_forcings_utils.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/test/src/test_fm_external_forcings_utils.f90
@@ -6,6 +6,24 @@ module test_fm_external_forcings_utils
 contains
 
     !$f90tw TESTCODE(TEST, test_fm_external_forcings_utils,
+    !$f90tw test_get_constituent_name__ignore_discharge, test_get_constituent_name__ignore_discharge,
+    subroutine test_get_constituent_name__ignore_discharge() bind(C)
+        use fm_external_forcings_utils, only: get_constituent_name
+
+        integer, parameter :: STRBUFLEN = 256
+        character(len=STRBUFLEN) :: constituent_name, base_quantity
+        
+        base_quantity = "don't touch me"
+
+        ! Discharge is not a constituent so this call should leave `contituent_name` unchanged and `base_quantity` empty.
+        call get_constituent_name("sourcesink_discharge", constituent_name, base_quantity)
+
+        call F90_ASSERT_STREQ(cstr(constituent_name), cstr(""), cstr("Unexpected constituent_name"))
+        call F90_ASSERT_STREQ(cstr(base_quantity), cstr("don't touch me"), cstr("Unexpected base_quantity"))
+    end subroutine test_get_constituent_name__ignore_discharge
+    !$f90tw)
+
+    !$f90tw TESTCODE(TEST, test_fm_external_forcings_utils,
     !$f90tw test_get_constituent_name__with_delta, test_get_constituent_name__with_delta,
     subroutine test_get_constituent_name__with_delta() bind(C)
         use fm_external_forcings_utils, only: get_constituent_name
@@ -48,6 +66,21 @@ contains
         call F90_ASSERT_STREQ(cstr(constituent_name), cstr("Bar"), cstr("Unexpected constituent_name"))
         call F90_ASSERT_STREQ(cstr(base_quantity), cstr("sourcesink_constituentDelta"), cstr("Unexpected base_quantity"))
     end subroutine test_get_constituent_name__with_delta__strip_sedfrac
+    !$f90tw)
+
+    !$f90tw TESTCODE(TEST, test_fm_external_forcings_utils,
+    !$f90tw test_get_constituent_name__without_delta, test_get_constituent_name__without_delta,
+    subroutine test_get_constituent_name__without_delta() bind(C)
+        use fm_external_forcings_utils, only: get_constituent_name
+
+        integer, parameter :: STRBUFLEN = 256
+        character(len=STRBUFLEN) :: constituent_name, base_quantity
+        
+        call get_constituent_name("sourcesink_sedFracBar", constituent_name, base_quantity)
+
+        call F90_ASSERT_STREQ(cstr(constituent_name), cstr("Bar"), cstr("Unexpected constituent_name"))
+        call F90_ASSERT_STREQ(cstr(base_quantity), cstr("sourcesink_constituent"), cstr("Unexpected base_quantity"))
+    end subroutine test_get_constituent_name__without_delta
     !$f90tw)
 
 end module test_fm_external_forcings_utils
