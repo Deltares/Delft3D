51817d137ce413bc3eb6527d1bb3f53ab5d89bf3
Michelle Jeuken <michelle.jeuken@deltares.nl>
2026-07-06 13:05:13 +0200

    DELWAQ-1256 Redirect output in the hope to see something on checkhydbal failing to run

--- test/deltares_testbench/thirdparty/checkhydbal/win64/run_checkhydbal.bat
+++ test/deltares_testbench/thirdparty/checkhydbal/win64/run_checkhydbal.bat
@@ -11,7 +11,7 @@ set PATH=%bindir%;%PATH%
 
 echo "    bin dir           : %bindir%"
 
-echo executing in this window: "%bindir%\checkhydbal.exe" %*
-"%bindir%\checkhydbal.exe" %*
+echo executing in this window: "%bindir%\checkhydbal.exe" %* > checkhydbal.log 2>&1
+"%bindir%\checkhydbal.exe" %* > checkhydbal.log 2>&1
 
 rem pause
