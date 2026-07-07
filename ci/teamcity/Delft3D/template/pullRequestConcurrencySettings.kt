4fb66d2beb24439473d857323a15b54e6a266729
sayed <ahmad.elsayed@deltares.nl>
2026-07-07 14:43:15 +0200

    Add temporary rule for current dev branch

--- ci/teamcity/Delft3D/template/pullRequestConcurrencySettings.kt
+++ ci/teamcity/Delft3D/template/pullRequestConcurrencySettings.kt
@@ -6,6 +6,9 @@ import jetbrains.buildServer.configs.kotlin.BuildsLimitingMode
 object TemplatePullRequestConcurrency : Template({
     name = "Pull Request Concurrency Settings"
 
-    maxRunningBuildsPerBranch = "pull/*:1"
+    maxRunningBuildsPerBranch = """
+        pull/*:1
+        tc/task/DEVOPSCICD-7-limit-pr-build-concurrency:1
+    """.trimIndent()
     runningBuildsLimitingMode = BuildsLimitingMode.CANCEL_OLDEST_RUNNING_BUILD
 })
