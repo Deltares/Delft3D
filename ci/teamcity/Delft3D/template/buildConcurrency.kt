94fa9c9ea822e908dccf20161ebfe1070576ebb5
sayed <ahmad.elsayed@deltares.nl>
2026-07-09 12:54:25 +0200

    Remove dev branch from concurrency rules

--- ci/teamcity/Delft3D/template/buildConcurrency.kt
+++ ci/teamcity/Delft3D/template/buildConcurrency.kt
@@ -7,10 +7,8 @@ object TemplateBuildConcurrency : Template({
     name = "Build Concurrency Settings"
 
     // add branch-specific rules as needed
-    // do not forget to remove dev branch!!
     maxRunningBuildsPerBranch = """
         pull/*:1
-        tc/task/DEVOPSCICD-7-limit-pr-build-concurrency:1
     """.trimIndent()
 
     runningBuildsLimitingMode = BuildsLimitingMode.CANCEL_OLDEST_RUNNING_BUILD
