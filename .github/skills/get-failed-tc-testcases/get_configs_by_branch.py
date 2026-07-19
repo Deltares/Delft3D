65dee01fe3a28aeb44477fb71c9238c73154da4c
René van Klink <rene.vanklink@deltares.nl>
2026-07-19 13:12:00 +0000

    Start using 'teamcity' command line tool instead of MCP server

--- .github/skills/get-failed-tc-testcases/get_configs_by_branch.py
+++ .github/skills/get-failed-tc-testcases/get_configs_by_branch.py
@@ -1,22 +1,31 @@
 import argparse
-import re
 from pathlib import Path
 import json
 import sys
+from typing import TextIO
 
 DEFAULT_TESTBENCH_TABLE_CSV: Path = Path("ci/teamcity/Delft3D/vars/dimr_testbench_table.csv")
 
-def validate_branch_name(value: str) -> str:
-    branch_name = value.strip()
-    mo = re.match(r"^(?P<engine>[a-z]+)/[a-z]+/[A-Z]+-[0-9]+[-\w]*$", branch_name)
-    if mo is None:
-        raise ValueError("Not a valid branch name")
-    return branch_name
+
+def make_sub_build_names_to_xml_paths(lines: TextIO, column_name: str) -> dict[str, Path]:
+    first_line = next(lines)
+    column_index = next((i for i, col in enumerate(first_line.split(",")) if col.strip() == column_name), None)
+    if column_index is None:
+        raise ValueError(f"'{column_name}' matches no columns in the TestBench table.")
+    if column_index < 2:
+        raise ValueError("The first two columns are reserved for config name and config path.")
+
+    splits = (line.split(",", maxsplit=column_index+1) for line in lines if line.strip())
+    return {
+        f"[{split[0].strip()}]": configs_dir / split[1].strip() for split in splits 
+        if platform in split[1] and split[column_index].strip().lower() == "true"
+    }
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--branch-name", required=True, type=validate_branch_name)
     parser.add_argument("--platform", required=True, choices=["win64", "lnx64"])
+    parser.add_argument("--branch-name", default="main")
     parser.add_argument("--configs-dir", type=Path, default=Path("configs"))
     parser.add_argument("--testbench-table-csv", type=Path, default=DEFAULT_TESTBENCH_TABLE_CSV)
     args = parser.parse_args()
@@ -26,20 +35,11 @@ if __name__ == "__main__":
     configs_dir: Path = args.configs_dir
     testbench_table_csv: Path = args.testbench_table_csv
 
-    build_config = branch_name.split("/", maxsplit=1)[0] + "-testbench"
+    idx = next((i for i, c in enumerate(branch_name) if c == '/'), None)
+    column_name = ("all" if idx is None else branch_name[:idx]) + "-testbench"
+
     with testbench_table_csv.open("r") as lines:
-        first_line = next(lines)
-        column_index = next((i for i, col in enumerate(first_line.split(",")) if col.strip() == build_config), None)
-        if column_index is None:
-            raise ValueError(f"Build config '{build_config}' matches no testbench configs.")
-        if column_index < 2:
-            raise ValueError("The first two columns are reserved for config name and config path.")
+        sub_builds_to_xml_paths = make_sub_build_names_to_xml_paths(lines, column_name)
 
-        splits = (line.split(",", maxsplit=column_index+1) for line in lines if line.strip())
-        name_to_config: dict[str, Path] = {
-            split[0].strip(): configs_dir / split[1].strip() for split in splits 
-            if platform in split[1] and split[column_index].strip().lower() == "true"
-        }
-
-    json.dump(name_to_config, fp=sys.stdout, default=str, indent=4)
+    json.dump(sub_builds_to_xml_paths, fp=sys.stdout, default=str, indent=4)
     print()
