0d5deeb5b99ce987de366680658fcff7767ed8d4
René van Klink <rene.vanklink@deltares.nl>
2026-07-23 11:37:42 +0200

    UNST-10125: UV_INDEX_URL now points to the internal-artifacts.deltares.nl instead of artifacts.deltares.nl

--- ci/teamcity/Delft3D/windows/testEnvironment.kt
+++ ci/teamcity/Delft3D/windows/testEnvironment.kt
@@ -65,9 +65,6 @@ object WindowsTestEnvironment : BuildType({
                     containers.deltares.nl/delft3d-dev/test/delft3d-test-environment-windows:%container.tag%
                 """.trimIndent()
             }
-            conditions {
-                matches("trigger.type", "vcs|schedule")
-            }
         }
     }
 
