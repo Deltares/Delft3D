e9a25c818a38fd4bd3148fd94a06f005136814f8
Koen Bussemaker <koen.bussemaker@deltares.nl>
2026-07-29 15:32:29 +0200

    UNST-10201 Added temporary default value if the spec is missing one

--- src/utils_lgpl/dflowfm_io/dflowfm_io/src/MduData.cpp
+++ src/utils_lgpl/dflowfm_io/dflowfm_io/src/MduData.cpp
@@ -7,6 +7,44 @@
 
 namespace dflowfm_io
 {
+    namespace
+    {
+        // TODO this is a temporary solution to provide default values for properties that do not have a default value
+        // defined in the schema. Eventually every property should have a default value defined in the schema, and this
+        // function can be removed.
+        std::string GetDummyDefault(const PropertySchema& schema)
+        {
+            switch (schema.value_type)
+            {
+                case ValueType::String:
+                    return "";
+                case ValueType::Int:
+                    return "0";
+                case ValueType::Float:
+                    return "0.0";
+                case ValueType::IntBool:
+                    return "false";
+                case ValueType::Path:
+                    return std::filesystem::path().string();
+                case ValueType::DateTime:
+                    return "20010101000000";
+                case ValueType::StringList:
+                    return "";
+                case ValueType::PathList: 
+                    return "";
+                case ValueType::FloatList:
+                    return "";
+                case ValueType::Enum:
+                    return schema.enum_values.empty() ? "0" : schema.enum_values.front().label;
+                case ValueType::IntEnum:
+                    return schema.enum_values.empty() ? "0" : std::to_string(schema.enum_values.front().value);
+                default:
+                    throw std::logic_error(std::format("Unhandled ValueType for property '{}'.", schema.key));
+            }
+        }
+    }
+
+
     MduData MduData::CreateFromSchema(const MduSchema& schema)
     {
         decltype(data_entries) entries;
@@ -17,12 +55,13 @@ namespace dflowfm_io
 
             for (const auto& propertySchema : sectionSchema.properties)
             {
-                if (propertySchema.default_value.empty()) continue;
-
                 if (schema.IsObsolete(propertySchema, propertySchema.default_value)) continue;
 
+                const std::string default_value = propertySchema.default_value.empty() ? GetDummyDefault(propertySchema)
+                                                                          : propertySchema.default_value;
+
                 std::optional<Value> value =
-                    MduValueConverter::FromString(propertySchema, propertySchema.default_value);
+                    MduValueConverter::FromString(propertySchema, default_value);
 
                 if (!value.has_value())
                 {
