4c40cb34ffb8ca2ec8de688b297c39e0142b3b5c
Mostafa Farrag <moah.farag@gmail.com>
2026-07-29 16:25:39 +0200

    move the switches to a higher level cmake in the dflowfm_io dir

---
+++ src/utils_lgpl/dflowfm_io/cmake/options.cmake
@@ -0,0 +1,6 @@
+# Options to switch on/off which wrapper is built by default with the whole project
+
+option(DFLOWFM_IO_BUILD_WRAPPER_PACKAGES
+	"Build the redistributable wrapper packages (Python wheel, C# NuGet) as part of the default build"
+	OFF
+)
