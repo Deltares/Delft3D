a631fa191432c72e5a18ae69c1148ea58ddccbfa
Robin van Westrenen <robin.vanwestrenen@deltares.nl>
2026-08-06 11:37:01 +0200

    Move over dvc checkout code for documentation from old branch.

---
+++ ci/teamcity/Delft3D/template/downloadFromDVC.kt
@@ -0,0 +1,29 @@
+package Delft3D.template
+
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.*
+import jetbrains.buildServer.configs.kotlin.triggers.*
+
+object TemplateDownloadFromDVC : Template({
+    name = "Download doc folders."
+    description = "Download all doc folders via doc.dvc files, recursively for engine dir."
+    buildNumberPattern = "%build.vcs.number%"
+
+    params {
+        // DVC remote credentials (s3://delft3d-testbench)
+        param("env.AWS_ACCESS_KEY_ID", "%dvc_testbench_accesskey%")
+        password("env.AWS_SECRET_ACCESS_KEY", "%dvc_testbench_secret%")
+    }
+
+    steps {
+        script {
+            name = "split engine_name_and_dir"
+            scriptContent = "call ci/teamcity/Delft3D/windows/scripts/extractEngineNameAndDir.bat %engine_name_and_dir%"
+        }
+        script {
+            name = "DVC Pull all doc.dvc files recursively"
+            scriptContent = """powershell.exe -NoProfile -ExecutionPolicy Bypass -File "ci/teamcity/Delft3D/windows/scripts/pullDocsForEngine.ps1" -EngineDir "%engine_dir%" """
+        }
+    }
+})
