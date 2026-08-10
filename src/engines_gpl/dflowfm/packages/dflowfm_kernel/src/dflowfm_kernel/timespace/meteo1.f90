f4e6559f4016285d71ec405c10f1d9d7f4752b98
FlorisBuwaldaDeltares <117371974+FlorisBuwaldaDeltares@users.noreply.github.com>
2026-08-10 12:52:15 +0300

    Fm/task/unst 9008 fix waq newext (#1147)
    * properly pass 2d slice of 3d array, as indexing at first_index fails.
    
    Fix unc_loc_global for waqfunction, added unit test
    
    * register the waqfunctions similar to laterals, sourcesinks etc to avoid ec module pointer invalidation
    
    * now preregister all 4 waq quantities (waqparameter, waqsegmentnumber, waqfunction, waqsegmentfunction)
    
    * support waq mass balance area
    
    * support both massbalancearea and waqmassbalancearea
    
    * fix default polygon method, fix accidental intent out zu, pre-copy painp for waqparameter and waqsegmentnumber, flowgeom_mask sets mask to 1 for invalid spatial location. added unit tests
    
    * map blockfrom to justupdate to provide compatibility for legacy method 0.
    
    Update right part of painp for 3d input
    
    * add special waqbot 3D handling
    
    * update e02_f030_c032_frisianinlet_waqbot_restart
    
    * update e02_f030_c033_frisianinlet_waqbot_restart_z
    
    * update e02_f030_c100_spatial_parameters_2D
    
    * update e02_f030_c102_temporal_parameters
    
    * update e02_f030_c110_frisianinlet_segment_function
    
    * update e02_f030_c101_spatial_parameters_3D
    
    * replace global x,y with a single dummy coordinate
    
    * add some comments
    
    * properly initialize vertical position.
    New references for waqsegmentfunction case because it used a stale cache file
    
    * delete erroneously committed initialization of vertical_position
    
    * for some annoying reason the vertical_position initialization got reverted :(
    
    * add waq segment post initialization processing (finalize_waq_spatial_fields)
    
    * did you know waqsegmentnumber has 16 characters, not 15?
    
    * pass quantity_base and quantity_specific instead of re-splitting inside finish_mass_balance_area_target
    
    * remove parsing waq quantities from meteo blocks. split single line statement.
    
    * vertical position => target layer, kbot => bottom
    
    * remove blockfrom, change docstrings
    
    * add missing docstrings
    
    * remove obsolete unit test
    
    * support .bc for waqfunction
    
    * added full quantity to finish_mass_balance_area_target for error messaging. Split copilot's one-line if statements. Added some docs. 'kbot'=> 'bottom' in error message for targetLayer

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/timespace/meteo1.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/timespace/meteo1.f90
@@ -3262,13 +3262,22 @@ contains
       use m_filez, only: oldfil, doclose, newfil
 
       ! Arguments
-      real(kind=dp), dimension(nx), intent(in) :: xu
-      real(kind=dp), dimension(nx), intent(in) :: yu
-      real(kind=dp), dimension(nx), intent(out) :: zu
+
       integer, intent(in) :: nx
-      character(*), intent(in) :: filename !< file name for meteo data file
-      integer, intent(in) :: filetype !< spw, arcinfo, uniuvp etc
-      integer, intent(in) :: method !< time/space interpolation method
+
+      real(kind=dp), intent(in) :: xu(nx)
+      real(kind=dp), intent(in) :: yu(nx)
+      real(kind=dp), intent(inout) :: zu(nx)
+
+      character(*), intent(in) :: filename ! file name for meteo data file
+      integer, intent(in) :: filetype ! spw, arcinfo, uniuvp etc
+      integer, intent(in) :: method ! time/space interpolation method
+      ! 4 : inside polygon
+      ! 5 : triangulation
+      ! 6 : averaging
+      ! 7 : index triangulation
+      ! 8 : smoothing
+      ! 9 : internal diffusion
       integer, intent(in) :: operand
       real(kind=dp), dimension(:), intent(in) :: transformcoef !< Transformation coefficients
       integer, intent(in) :: iprimpos !< only needed for averaging, position of primitive variables in network
