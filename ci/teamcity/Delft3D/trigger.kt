4b896a18c13072b8219622e4be5e1a207c6c50b0
Sam Vente <sam.vente@deltares.nl>
2026-08-11 12:40:27 +0200

    fix: convert tabs to spaces

--- ci/teamcity/Delft3D/trigger.kt
+++ ci/teamcity/Delft3D/trigger.kt
@@ -278,13 +278,12 @@ object Trigger : BuildType({
         script {
             name = "Start DVC diff report build"
 
-	    // it's taking up too many TC resources, so this is disabled until we can 
-	    // diagnose and fix that
-	    enabled = false
+            // it's taking up too many TC resources, so this is disabled until we can 
+            // diagnose and fix that
+            enabled = false
             conditions {
                 doesNotContain("teamcity.build.triggeredBy", "Snapshot dependency")
                 startsWith("teamcity.build.branch", "pull")
-
             }
 
             scriptContent = """
