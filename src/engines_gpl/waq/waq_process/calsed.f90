f81e806bba8ff51ef771bc5af3197c5db3ae17a9
Arjen Markus <arjen.markus@deltares.nl>
2026-09-01 15:38:34 +0200

    Correct use of the exponent

--- src/engines_gpl/waq/waq_process/calsed.f90
+++ src/engines_gpl/waq/waq_process/calsed.f90
@@ -159,7 +159,7 @@ contains
 
                 fpom = 1.0
                 if ( usepom ) then
-                    fpom = max( 1.0, ( pom / pom_crit ) ** pom_crit )
+                    fpom = max( 1.0, ( pom / pom_crit ) ** pom_exp )
                 endif
 
                 !     Bereken VSED
