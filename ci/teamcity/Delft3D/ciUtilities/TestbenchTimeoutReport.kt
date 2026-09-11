4d3a77dc72224a8a1b01bb38a83f1030a6355b9e
Robin van Westrenen <robin.vanwestrenen@deltares.nl>
2026-09-11 11:55:56 +0200

    Fix Kotlin syntax.

--- ci/teamcity/Delft3D/ciUtilities/TestbenchTimeoutReport.kt
+++ ci/teamcity/Delft3D/ciUtilities/TestbenchTimeoutReport.kt
@@ -78,7 +78,7 @@ object TestbenchTimeoutReport : BuildType({
                     --csv "%teamcity.build.checkoutDir%/ci/teamcity/Delft3D/vars/dimr_testbench_table.csv" \
                     --output-dir "%teamcity.build.checkoutDir%/timeout-report" \
                     --report-url "%env.TEAMCITY_SERVER_URL%/repository/download/%system.teamcity.buildType.id%/%teamcity.build.id%:id/timeout-report/report.html" \
-                    "${email_args[@]}"
+                    "${'$'}{email_args[@]}"
             """.trimIndent()
             dockerImage = "%docker_image%"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
