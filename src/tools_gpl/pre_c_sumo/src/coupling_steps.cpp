c29f5acc00a1aa1b22116d673621110fc068f2bb
Erwin Mulder <erwin.mulder@deltares.nl>
2026-09-16 15:32:43 +0200

    UNST-101332: Fixed indexing while copying velocity from 3d mesh in preC-SUMO.

--- src/tools_gpl/pre_c_sumo/src/coupling_steps.cpp
+++ src/tools_gpl/pre_c_sumo/src/coupling_steps.cpp
@@ -33,8 +33,8 @@ namespace pre_c_sumo
                 // 3d.coordinates = (x1, y1, z1, x2, y2, z2, ...): skip "index_3d + i" points, then skip x and y
                 .z_coordinate = mesh_3d.coordinates[(index_3d + i) * 3 + 2],
                 // 3d.velocities = (vx1, vy1, vz1, vx2, vy2, vz2, ...): skip "index_3d + i" points, then get x and y
-                .x_velocity = mesh_3d.quantities[flow_velocities_id][(index_3d + 1) * 3 + 0],
-                .y_velocity = mesh_3d.quantities[flow_velocities_id][(index_3d + 1) * 3 + 1],
+                .x_velocity = mesh_3d.quantities[flow_velocities_id][(index_3d + i) * 3 + 0],
+                .y_velocity = mesh_3d.quantities[flow_velocities_id][(index_3d + i) * 3 + 1],
                 .density = mesh_3d.quantities[densities_id][index_3d + i],
                 .constituents = {0.0, 0.0, 0.0}, // constituents, // TODO: obtain layered data from far-field
             });
