dd845ea5cbee543e244d7d0b874d6dad219f5b06
Michelle Jeuken <michelle.jeuken@deltares.nl>
2026-09-17 16:05:28 +0200

    DELWAQ-1297 Add limiters for water quality substances and other tracers in D-Flow FM

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute_transport/alloc_transport.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute_transport/alloc_transport.f90
@@ -130,6 +130,8 @@ contains
       call realloc(itrac2const, numtracers, keepExisting=KeepExisting, fill=0)
       call realloc(ifrac2const, numfracs, keepExisting=KeepExisting, fill=0)
 
+! tracer mass limited
+      call realloc(maserrtra, numtracers, keepExisting=KeepExisting, fill=0.0_dp)
       if (jawaqproc > 0) then
 !     WAQ
          call realloc(isys2const, num_substances_total, keepExisting=.true., fill=0)
