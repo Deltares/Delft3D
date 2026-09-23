7fcd681fe8624848ef792b6e211656192013b1b3
thomas-pijls <thomas.pijls@deltares.nl>
2026-09-23 11:50:31 +0200

    UNST-10187: Fixed typo

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/s1ini.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/s1ini.f90
@@ -76,7 +76,7 @@ contains
       end if
 
       if (temperature_model == TEMPERATURE_MODEL_TRANSPORT) then
-         heatsrc = 0.0_dp ! just prior to asource_sinks%update_discharges that may add to heatsrc
+         heatsrc = 0.0_dp ! just prior to source_sinks%update_discharges that may add to heatsrc
       else if (temperature_model == TEMPERATURE_MODEL_EXCESS .or. temperature_model == TEMPERATURE_MODEL_COMPOSITE) then
          heatsrc = heatsrc0 ! heatsrc0 established in heatu at interval usertimestep
       end if
@@ -270,7 +270,7 @@ contains
          end if
 
          if (source_sinks%num_total > 0) then
-            call asource_sinks%update_discharges() ! add sources and sinks
+            call source_sinks%update_discharges() ! add sources and sinks
          end if
 
          if (wrwaqon) then ! Update waq output
